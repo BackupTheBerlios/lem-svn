9f1da5e937f1bfe187461405186c3ae2ecf30c10
u4128551 <u4128551@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-05-02 07:04:09 +0000

    Added file to retain last directory accesed.

---
+++ trunk/src/verifier/WorkSpace.java
@@ -0,0 +1,26 @@
+/*
+ * WorkSpace.java
+ *
+ * Created on 2 May 2005, 06:31
+ */
+
+package verifier;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Administrator
+ */
+public class WorkSpace implements Serializable{
+    String CurrentWorkspace;
+    /** Creates a new instance of WorkSpace */
+    public WorkSpace(String inWorkspace) {
+        CurrentWorkspace = inWorkspace;
+    }
+    public String getWorkSpace()
+    {
+        return CurrentWorkspace;
+    }
+    
+}
