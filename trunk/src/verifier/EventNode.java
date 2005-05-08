b0a65010580bf5518ffe240759d3e2b17b553402
u3958283 <u3958283@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-05-08 03:27:58 +0000

    New classes to represent EventTree and Events corresponding to the notion of Signlas in xtUML.

---
+++ trunk/src/verifier/EventNode.java
@@ -0,0 +1,32 @@
+/*
+ * EventNode.java
+ *
+ * Created on 8 May 2005, 11:52
+ */
+
+package verifier;
+
+import metamodel.Event ;
+/**
+ *
+ * @author SHukuBOy
+ */
+public class EventNode extends AbstractDescriptionNode {
+    
+    Event event ; 
+    /** Creates a new instance of EventNode */
+    public EventNode(Event e) {
+        event = e ; 
+    }
+    
+    public String toString() {
+        return "Event: " + event.getName() ;  
+    }
+    
+    public String getDescription() {
+         if (event.getDescription() != null ) 
+            return trim(event.getDescription());   
+        else 
+            return "" ;         
+    }
+}
