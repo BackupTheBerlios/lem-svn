3e45ecbcbfa7ddd8451337959621ac667882aa4c
u3957053 <u3957053@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-05-14 23:09:12 +0000

    Add an Action abstract class

---
+++ trunk/src/metamodel/Action.java
@@ -0,0 +1,24 @@
+/*
+ * Action.java
+ *
+ * Created on May 14, 2005, 9:56 AM
+ *
+ * To change this template, choose Tools | Options and locate the template under
+ * the Source Creation and Management node. Right-click the template and choose
+ * Open. You can then make changes to the template in the Source Editor.
+ */
+
+package metamodel;
+
+/**
+ *
+ * @author sjr
+ */
+public abstract class Action {
+    
+    /** Creates a new instance of Action */
+    public Action() {
+    }
+    
+    public abstract void execute();
+}
