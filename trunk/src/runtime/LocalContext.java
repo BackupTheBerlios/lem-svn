d661c38d47b8ca2b54f4e8f3cd3f164e68a5d126
u4128551 <u4128551@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-05-17 04:51:24 +0000

    Added local context class.

---
+++ trunk/src/runtime/LocalContext.java
@@ -0,0 +1,20 @@
+/*
+ * LocalContext.java
+ *
+ * Created on 17 May 2005, 14:47
+ */
+
+package runtime;
+
+/**
+ *
+ * @author u4128551
+ */
+public class LocalContext extends Context{
+    
+    /** Creates a new instance of LocalContext */
+    public LocalContext() {
+        super();
+    }
+    
+}
