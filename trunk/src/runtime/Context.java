a083e5bb82ce8fee55046948410897c6de7d5eb7
u3957053 <u3957053@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-05-17 05:02:38 +0000

    Added Javadoc comments, fix getLocalVariables

--- trunk/src/runtime/Context.java
+++ trunk/src/runtime/Context.java
@@ -9,25 +9,50 @@ package runtime;
 import java.util.ArrayList;
 
 /**
- *
+ * Every Action in LEM executes in some sort of "context".
  * @author u4128551
  */
 public class Context {
     ArrayList objectList = null;
     ArrayList localVariables = null;
     Context parentContext = null;
-    /** Creates a new instance of Context */
+
+    /**
+     * Creates a new instance of Context. Contexts are hierarchical,
+     * so the parent context is passed in as a parameter.
+     * @param inContext the parent context
+     */
     public Context(Context inContext) {
         parentContext = inContext;
     }
-    public void addObject(runtime.Object inObject)
-    {}
-    public ArrayList getObjects()
-    {
+    /**
+     * Adds the given object to this context
+     * @param inObject the object to add to the context
+     */
+    public void addObject(runtime.Object inObject) {
+    }
+    
+    /**
+     * Returns the list of objects visible in this context
+     * @return the objects in this context
+     */
+    public ArrayList getObjects() {
         return objectList;
     }
-    public ArrayList getLocalVariables()
-    {}
-    public void finish()
-    {}
+    
+    /**
+     * Returns the list of local variables in this context
+     * @return the local variables in this context
+     */
+    public ArrayList getLocalVariables() {
+        return localVariables;
+    }
+    
+    /**
+     * Tells this context that is about to finish. The context should check
+     * all visible objects for participation in all relevant associations,
+     * etc.
+     */
+    public void finish() {
+    }
 }
