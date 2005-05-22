b6965ad9f3a58d1ef248dec441292376afdb8656
u3957053 <u3957053@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-05-22 07:02:19 +0000

    Now places Relationships at correct level in tree; Now handles the 'Default Subsystem' correctly

--- branches/toshio-proto/src/verifier/SubsystemNode.java
+++ branches/toshio-proto/src/verifier/SubsystemNode.java
@@ -16,36 +16,41 @@ import metamodel.Domain;
 import metamodel.Subsystem;
 
 public class SubsystemNode extends AbstractDescriptionNode {
-	
-	Subsystem subsystem;
-	
-	/** Creates a new instance of SubsystemNode */
-	public SubsystemNode( Subsystem s ) {
-		this.subsystem = s;
-		
-		DefaultMutableTreeNode classLevel = new DefaultMutableTreeNode( "Classes" );
-		
-		Iterator i = s.getClasses().values().iterator();
-		while (i.hasNext()) {
-			classLevel.add( new ClassNode( (metamodel.Class)i.next() ));
-		}
-		
-		add( classLevel );
-	}
-	
-	public String toString() {
-		return "Subsystem " + subsystem.getName();
-	}
-	
-	public String getDescription() {
-		if (subsystem.getDescription() != null)
-			return trim(subsystem.getDescription());
-		else
-			return "" ;
-	}
-	
-	public JPopupMenu getContextMenu() {
-		JPopupMenu ContextMenu = new JPopupMenu();
-		return ContextMenu;
-	}
+    
+    Subsystem subsystem;
+    
+    /** Creates a new instance of SubsystemNode */
+    public SubsystemNode( Subsystem s ) {
+        this.subsystem = s;
+        
+        if (s.getClasses().size() == 0) {
+            DefaultMutableTreeNode classLevel = new DefaultMutableTreeNode( "[No Classes]" );
+            add( classLevel );
+        } else {
+            DefaultMutableTreeNode classLevel = new DefaultMutableTreeNode( "Classes" );
+            
+            Iterator i = s.getClasses().values().iterator();
+            while (i.hasNext()) {
+                classLevel.add( new ClassNode( (metamodel.Class)i.next() ));
+            }
+            
+            add( classLevel );
+        }
+    }
+    
+    public String toString() {
+        return "Subsystem " + subsystem.getName();
+    }
+    
+    public String getDescription() {
+        if (subsystem.getDescription() != null)
+            return trim(subsystem.getDescription());
+        else
+            return "" ;
+    }
+    
+    public JPopupMenu getContextMenu() {
+        JPopupMenu ContextMenu = new JPopupMenu();
+        return ContextMenu;
+    }
 }
