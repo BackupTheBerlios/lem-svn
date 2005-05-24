f49941be978379672f99fc3fe682b345cc5f0b86
u3957053 <u3957053@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-05-24 01:07:06 +0000

    Add Javadoc comments

--- trunk/src/metamodel/CreateAction.java
+++ trunk/src/metamodel/CreateAction.java
@@ -12,27 +12,41 @@ package metamodel;
 import runtime.*;
 
 /**
- *
+ * This Action is responsible for creating new instances of runtime.Object.
+ * This action takes a list of
  * @author sjr
  */
 public class CreateAction extends Action {
     
     /**
-     * A new instance of this class will be created when this CreateAction is
-     * executed
+     * A new object containing an instance of this class will be created when this CreateAction is
+     * executed.
      *
-     * TODO: An instance may be an instance of multiple classes
      */
     metamodel.Class theClass = null;
+    // TODO: An object may be an instance of multiple classes
+    
     
     /** Creates a new instance of CreateAction */
     public CreateAction() {
     }
     
+    /**
+     * Sets the class to be instantiated when this action is executed
+     *
+     * @param theClass the class to be instantiated
+     */
     public void setClass( metamodel.Class theClass ) {
         this.theClass = theClass;
     }
+    // TODO: An object may be an instance of multiple classes
     
+    /**
+     * Execute the given action. This will cause a new Object to be created and placed
+     * in the context's object list
+     * 
+     * @param context The context into which the newly created object should be inserted
+     */
     public void execute( Context context ) {
         runtime.Object o = new runtime.Object( new metamodel.Class[] {theClass} );
         context.addObject(o);
