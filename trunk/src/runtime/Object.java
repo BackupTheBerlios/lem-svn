196b55238b5cca795a51d716f3b0eb7732b1e069
u3957053 <u3957053@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-05-31 13:03:18 +0000

    When instantiating a new Object, check to see that the set of classes specified conform to the generalisation hierarchy which the domain demands
    NOTE: The getParents() method is almost certainly flawed. I doubt it excludes
    the root of a hierarchy when it checks for duplicate parents.

--- trunk/src/runtime/Object.java
+++ trunk/src/runtime/Object.java
@@ -6,23 +6,102 @@
 
 package runtime;
 
-import metamodel.DataType;
-    
+import java.util.HashMap;
+import java.util.Collection;
+import java.util.Iterator;
+import metamodel.*;
+
 /**
+ * This class represents a LEM runtime object. When the model is being executed,
+ * instances of this class and deleted via the 'create' and 'delete' action
+ * language statements.
  *
  * @author u4128551
+ * @author u3957053
+ * @see http://xtuml.jdns.org/wiki/index.php/Runtime_object
  */
 public class Object {
-    InstanceAttribute attributes[] = null;
-    metamodel.Class classes[] = null;
+//    InstanceAttribute attributes[] = null;
+    Collection instances = null;
     
-    /** Creates a new instance of Object */
-    public Object(metamodel.Class [] inClasses) {
-        classes=inClasses;
+    /**
+     * Creates a new instance of Object. The object will contain instances
+     * of all the classes in the <code>classes</code> collection.
+     *
+     * @todo Could we pass a DataType in here?
+     * @param classes The classes which this object should
+     * @throws LemRuntimeException if the <code>classes</code> collection does
+     * not represent a valid type for the new object
+     */
+    public Object(Collection classes) throws LemRuntimeException {
+        
+        // Do these classes actually participate in the same inheritance
+        // hierarchy?
+        
+        Iterator i = classes.iterator();
+        HashMap h = new HashMap();
+        
+        while( i.hasNext() ) {
+            metamodel.Class c = (metamodel.Class)i.next();
+            
+            // If this class is abstract, you can't instantiate it
+            if( c.isAbstract() ) {
+                throw new LemRuntimeException( "Class '" + c.getName() + "' is abstract " +
+                        "and cannot be instantiated" );
+            }
+            
+            // Build up a list of this class' parents. If any class in the
+            // collection shares the same parent, we can't instantiate that
+            // class
+            
+            if( getParents( c, h )) {
+                throw new LemRuntimeException( "Class '" 
+                        + c.getName() + "' shares a common parent with another class" );
+            }
+            
+        }
     }
-
+    
+    /**
+     * @param attributeName the attribute to find
+     * @return the attribute with the given name, or <code>null</code> if the
+     * named attribute doesn't exist
+     */
     public InstanceAttribute getAttribute(String attributeName) {
         //insert code here :)
-        return attributes[0];
+//        return attributes[0];
+        return null;
+    }
+    
+    
+    /**
+     * Place all the parents of the given class into the given hashmap. This
+     * method traverses all subclass participation roles, including grandparent
+     * roles.
+     *
+     * @param c the class that we're finding parents for
+     * @param h the hash into which to insert the parent names
+     *
+     * @todo this is not the place for this method...
+     * @todo does this method exclude the root of a generalisation hierarchy 
+     * in the duplicate check?
+     */
+    private boolean getParents( metamodel.Class c, HashMap h ) {
+        boolean duplicates = false;
+        
+        HashMap superClasses = c.getSubclassParticipation();
+        if( superClasses.isEmpty() ) return duplicates;
+        
+        Iterator i = superClasses.values().iterator();
+        
+        while( i.hasNext() ) {
+            Generalisation g = (Generalisation)i.next();
+            metamodel.Class parent = g.getSuperClassRole().getParticipant();
+            if( h.containsKey( parent.getName() )) duplicates = true;
+            h.put( parent.getName(), parent );
+            return getParents( parent, h );
+        }
+        
+        return duplicates;
     }
 }
