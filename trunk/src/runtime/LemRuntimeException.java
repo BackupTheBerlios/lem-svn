cbebe342d00b8aafbde2a1d53f5b6ca86d691295
u3957053 <u3957053@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-05-31 13:01:34 +0000

    Add an exception which can be thrown by runtime objects to indicate runtime errors

---
+++ trunk/src/runtime/LemRuntimeException.java
@@ -0,0 +1,27 @@
+/*
+ * LemRuntimeException.java
+ */
+
+package runtime;
+
+import parser.Token;
+import java.util.*;
+import java.io.*;
+
+/**
+ * A general LEM runtime exception. It is expected that instances of this class
+ * will be thrown when something bad happens in a running model (eg, if an
+ * association constraint is violated).
+ *
+ * @author  sjr
+ */
+public class LemRuntimeException extends Exception  {
+    
+    /**
+     * Constructs an instance of <code>LemRuntimeException</code> with the specified detail message.
+     * @param msg the detail message.
+     */
+    public LemRuntimeException(String msg) {
+        super(msg);
+    }
+}
