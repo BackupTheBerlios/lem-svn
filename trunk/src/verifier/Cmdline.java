de5c557d4ebfc371d984ad8e613c54a808a88a61
u3293115 <u3293115@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-06-01 10:06:38 +0000

    Added basic command line model loader: my crappy java environment can't handle a fancy GUI. This will also be useful to start working on a regression test suite.

---
+++ trunk/src/verifier/Cmdline.java
@@ -0,0 +1,71 @@
+/**
+ * Cmdline.java
+ *
+ * Simple command line based model loader. Used for testing.
+ */
+
+package verifier;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.URL;
+import java.util.Vector;
+import metamodel.LemException;
+import metamodel.Model;
+import parser.ParseException;
+import tools.Lem;
+
+import java.io.*;
+
+
+/**
+ *
+ * @author  Nick Piggin
+ */
+public class Cmdline {
+    
+    Vector models = new Vector();
+    File workingDirectory = null;
+    
+    /** Creates new form Cmdline */
+    public Cmdline(String file) {
+        
+        try {
+		File selectedFile = new File(file);
+        	Model m = loadModel( selectedFile );
+                if( m != null )
+                    models.add( m );
+        } catch (FileNotFoundException fnfe) {
+		System.out.println("File not found");
+        } catch (Exception e) {
+		System.out.println("Exception");
+	}
+        
+/*        metamodel.Procedure p = m.getDomain( "Publications" ).getClass( "Manuscript" ).getStateMachine().getState("Adding").getProcedure();
+        runtime.ModelInstance i = new runtime.ModelInstance();
+        
+        p.execute(i); */
+    }
+
+    public Model loadModel( File modelFile ) throws FileNotFoundException, ParseException, LemException, IOException {
+        Lem l = new Lem();
+        FileInputStream fis = new FileInputStream( modelFile );
+        
+        return l.parse( fis );
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+	if (args.length != 1)
+		System.out.println("Incorrect usage");
+	else
+		new Cmdline(args[0]);
+    }
+}
