487ff444823eae335bc35ae6fe465d34cee14df9
u4128551 <u4128551@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-06-01 14:52:27 +0000

    Added Javadoc.

--- trunk/src/splash/Splasher.java
+++ trunk/src/splash/Splasher.java
@@ -8,11 +8,16 @@ package splash;
 
 
 /**
- *
+ * Splash window for eLEMinator application.
  * @author  David Gavin
  */
 
 public class Splasher {
+    /**
+     * Creates a new instance of Splasher. Splasher contains the amount of time
+     * that the splash window should be displayed (2000), the target class to be
+     * run (verifier.Eleminator) and the target image to be displayed (splash/eLEMinator.jpg).
+     */
     public static void main(String[] args) {
           SplashWindow spl;
           spl = new SplashWindow("splash/eLEMinator.jpg", new java.awt.Frame(), 2000); //Insert splash image here
