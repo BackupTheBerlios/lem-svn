21d93a8a9d0ac023cba8355e405553b768eefb59
u3957053 <u3957053@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-06-15 07:47:42 +0000

    Added ParentTest.lem and associated test case for metamodel.Object.validClasses in ObjectTest

--- trunk/src/test/runtime/ObjectTest.java
+++ trunk/src/test/runtime/ObjectTest.java
@@ -5,7 +5,17 @@
  * Created on June 15, 2005, 2:16 PM
  */
 
-package test.runtime;
+package runtime;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import metamodel.LemException;
+import metamodel.Model;
+import parser.ParseException;
+import tools.Lem;
 
 /**
  *
@@ -17,6 +27,52 @@ public class ObjectTest extends junit.framework.TestCase {
         super(testName);
     }
     
-    public void testBlah() {
+    protected void setUp() {
+    }
+    
+    /**
+     * Please refer to the regression/tests/ParentTest.lem
+     */
+    public void testValidClassesSimple() {
+        Lem l = new Lem();
+        Model m = null;
+        
+        try {
+            m = l.parse( new FileInputStream( "regression/tests/ParentTest.lem" ));
+        } catch( FileNotFoundException fnfe ) {
+            fail( "Could not find model file " + fnfe.getMessage() );
+        } catch( IOException e ) {
+            fail( "Could not read model file: " + e.getMessage() );
+        } catch( ParseException e ) {
+            fail( "Could not parse model file: " + e.getMessage() );
+        } catch( LemException e ) {
+            fail( "Some LEMException occurred: " + e.getMessage() );
+        }
+        
+        HashMap classes = m.getDomain("TestDomain").getClasses();
+        HashMap parents = new HashMap();
+        
+        metamodel.Class a = (metamodel.Class)classes.get( "A" );
+        metamodel.Class b = (metamodel.Class)classes.get( "B" );
+        metamodel.Class c = (metamodel.Class)classes.get( "C" );
+        metamodel.Class d = (metamodel.Class)classes.get( "D" );
+        metamodel.Class e = (metamodel.Class)classes.get( "E" );
+        
+        ArrayList classList = new ArrayList();
+        classList.add( b );
+        
+        assertEquals( "B is a valid object in and of itself", true, runtime.Object.validClasses( classList ));
+        
+        classList.add( e );
+        assertEquals( "B,E participate in the same hierarchy", false, runtime.Object.validClasses( classList ));
+        
+        classList.add( a );
+        assertEquals( "A is abstract", true, a.isAbstract() );
+        
+        classList.clear();
+        classList.add( b );
+        classList.add( d );
+        classList.add( e );
+        assertEquals( "B, D and E participate in the same hierarchy", false, runtime.Object.validClasses( classList ));
     }
 }
