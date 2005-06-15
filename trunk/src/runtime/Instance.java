4963df64bd8f1e3ee062cf86bf2d461952d8176c
u3957053 <u3957053@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-06-15 16:07:38 +0000

    add attributeInstance map, fix javadoc, add initialiseAttributeInstances() for the creation of empty AttributeInstance objects

--- trunk/src/runtime/Instance.java
+++ trunk/src/runtime/Instance.java
@@ -6,23 +6,68 @@
 
 package runtime;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import metamodel.Attribute;
 import metamodel.State;
-import metamodel.Class;
+
 
 /**
- *
+ * This class represents an instance of an xtUML Class. An Object is made up of
+ * one or more of these Instance instances.
+ * 
+ * @see the xtUML wiki page on Runtime Objects: http://xtuml.jdns.org/wiki/index.php/Runtime_object
+ * @author James Ring
  * @author u4128551
  */
 public class Instance {
+    /**
+     * The state of this instance
+     */
     State currentState = null;
-    Class instanceOfClass = null;
-    /** Creates a new instance of Instance */
-    public Instance(Class inClass) {
-        instanceOfClass = inClass;
-    }
-    public Instance(Class inClass, State inState) {
-        instanceOfClass = inClass;
-        currentState = inState;
+    
+    /**
+     * A map of attribute names to AttributeInstance references
+     */
+    Map attributeInstances = null;
+    
+    /**
+     * The Class of which this Instance is an instance
+     */
+    metamodel.Class instanceOfClass = null;
+    
+    /** 
+     * Creates a new instance of Instance given the template Class 
+     *
+     * @param theClass the class which is instantiated by this instance
+     */
+    public Instance(metamodel.Class theClass) {
+        instanceOfClass = theClass;
+        
+        initialiseAttributeInstances();
     }
     
+    /** 
+     * Creates a new instance of Instance given the template Class and an initial state
+     *
+     * @param theClass the class which is instantiated by this instance
+     * @param state the state in which this instance should begin
+     */
+    public Instance(metamodel.Class theClass, State state) {
+        instanceOfClass = theClass;
+        currentState = state;
+        
+        initialiseAttributeInstances();
+    }
+    
+    private void initialiseAttributeInstances() {
+        Iterator i = instanceOfClass.getAttributes().values().iterator();
+        attributeInstances = new HashMap();
+        
+        while( i.hasNext() ) {
+            Attribute a = (Attribute)i.next();
+            attributeInstances.put( a.getName(), new AttributeInstance(a));
+        }
+    }
 }
