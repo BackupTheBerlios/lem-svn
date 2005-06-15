06fc1898491b14bd0abe4364a583a890d6529adc
u3957053 <u3957053@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-06-15 04:45:18 +0000

    Added a test test...

---
+++ trunk/src/test/runtime/ObjectTest.java
@@ -0,0 +1,22 @@
+/*
+ * ObjectTest.java
+ * JUnit based test
+ *
+ * Created on June 15, 2005, 2:16 PM
+ */
+
+package test.runtime;
+
+/**
+ *
+ * @author sjr
+ */
+public class ObjectTest extends junit.framework.TestCase {
+    
+    public ObjectTest(String testName) {
+        super(testName);
+    }
+    
+    public void testBlah() {
+    }
+}
