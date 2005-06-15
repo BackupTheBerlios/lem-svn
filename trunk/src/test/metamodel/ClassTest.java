d70b3b9a6ff3e793c2e522bd3c5ef2bebf48313d
u3957053 <u3957053@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-06-15 14:42:44 +0000

    Add test file for metamodel.Class

---
+++ trunk/src/test/metamodel/ClassTest.java
@@ -0,0 +1,74 @@
+/*
+ * ClassTest.java
+ * JUnit based test
+ *
+ * Created on June 15, 2005, 8:04 PM
+ */
+
+package metamodel;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import parser.ParseException;
+import java.util.*;
+import junit.framework.TestCase;
+import tools.Lem;
+
+/**
+ *
+ * @author sjr
+ */
+public class ClassTest extends TestCase {
+    
+    public ClassTest(String testName) {
+        super(testName);
+    }
+
+    protected void setUp() throws Exception {
+    }
+
+    protected void tearDown() throws Exception {
+    }
+    
+    public void testGetAllGeneralisations() {
+        Lem l = new Lem();
+        Model m = null;
+        
+        try {
+            m = l.parse( new FileInputStream( "regression/tests/ParentTestComplex.lem" ));
+        } catch( FileNotFoundException fnfe ) {
+            fail( "Could not find model file " + fnfe.getMessage() );
+        } catch( IOException e ) {
+            fail( "Could not read model file: " + e.getMessage() );
+        } catch( ParseException e ) {
+            fail( "Could not parse model file: " + e.getMessage() );
+        } catch( LemException e ) {
+            fail( "Some LEMException occurred: " + e.getMessage() );
+        }
+        
+        HashMap classes = m.getDomain("TestDomain").getClasses();
+        HashMap gens = new HashMap();
+        
+        metamodel.Class a = (metamodel.Class)classes.get( "A" );
+        metamodel.Class b = (metamodel.Class)classes.get( "B" );
+        metamodel.Class c = (metamodel.Class)classes.get( "C" );
+        metamodel.Class d = (metamodel.Class)classes.get( "D" );
+        metamodel.Class e = (metamodel.Class)classes.get( "E" );
+        metamodel.Class f = (metamodel.Class)classes.get( "F" );
+        metamodel.Class g = (metamodel.Class)classes.get( "G" );
+        metamodel.Class h = (metamodel.Class)classes.get( "H" );
+        metamodel.Class i = (metamodel.Class)classes.get( "I" );
+        
+        assertEquals( "A participates as a subclass in 0 generalisations", 0, a.getAllGeneralisations().size() );
+        gens = b.getAllGeneralisations();
+        assertEquals( "B participates only in R1", true, gens.size() == 1 && gens.get("R1") != null );
+        gens.clear();
+        gens = i.getAllGeneralisations();
+        assertEquals( "I participates in R3 and R4", true, gens.size() == 2 && gens.get("R3") != null && gens.get( "R4" ) != null );
+        gens.clear();
+        gens = c.getAllGeneralisations();
+        assertEquals( "C participates in R1 only", true, gens.size() == 1 && gens.get( "R1" ) != null );
+    }
+    
+}
