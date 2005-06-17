efe6e34816857ed644183dd4c09c80fb884947b9
u3957053 <u3957053@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-06-17 04:47:35 +0000

    Contexts can now have LemEventListener objects registered to receive LemEvents

--- trunk/src/runtime/Context.java
+++ trunk/src/runtime/Context.java
@@ -7,16 +7,41 @@
 package runtime;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  * Every Action in LEM executes in some sort of "context".
  * @author u4128551
  */
 public class Context {
-    ArrayList objectList = new ArrayList();
-    ArrayList localVariables = null;
+    /** 
+     * A collection of all runtime objects known to this context
+     */
+    Collection objectList = new ArrayList();
+    
+    /**
+     * A collection of all variables local to the context
+     */
+    Collection localVariables = null;
+    
+    /**
+     * A collection of LemEventListener instances interested in 
+     * receiving notification of events occuring within this context.
+     */
+    Collection listeners = new ArrayList();
+
+    /**
+     * This context's parent.
+     */
     Context parentContext = null;
 
+    /**
+     * The nullary Context constructor. Creates a new Context with no objects,
+     * no listeners and a null list of local variables
+     */
+    public Context() {
+    }
+    
     /**
      * Creates a new instance of Context. Contexts are hierarchical,
      * so the parent context is passed in as a parameter.
@@ -37,7 +62,7 @@ public class Context {
      * Returns the list of objects visible in this context
      * @return the objects in this context
      */
-    public ArrayList getObjects() {
+    public Collection getObjects() {
         return objectList;
     }
     
@@ -45,7 +70,7 @@ public class Context {
      * Returns the list of local variables in this context
      * @return the local variables in this context
      */
-    public ArrayList getLocalVariables() {
+    public Collection getLocalVariables() {
         return localVariables;
     }
     
@@ -55,5 +80,30 @@ public class Context {
      * etc.
      */
     public void finish() {
+    }
+    
+    
+    /**
+     * Adds the given LemEventListener to the list of listeners. This listener
+     * will now receive LemEvent objects representing events that occur within this
+     * context
+     */
+    public void addLemEventListener( LemEventListener l ) {
+        listeners.add(l);
+    }
+    
+    /**
+     * Remove the given LemEventListener from the list of listeners. This listener
+     * will receive no more LemEvents
+     */
+    public void removeLemEventListener( LemEventListener l ) {
+        listeners.remove(l);
+    }
+    
+    /**
+     * Returns the collection of all LemEventListener objects registered with this context
+     */
+    public Collection getLemEventListeners() {
+        return listeners;
     }
 }
