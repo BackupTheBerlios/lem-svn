b90e2757dbf1b5f7bace2a09773f97a31e4220ba
u3957053 <u3957053@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-06-17 04:55:09 +0000

    Added Domain context

---
+++ trunk/src/runtime/DomainContext.java
@@ -0,0 +1,24 @@
+/*
+ * DomainContext.java
+ *
+ * Created on June 17, 2005, 12:52 PM
+ *
+ * To change this template, choose Tools | Options and locate the template under
+ * the Source Creation and Management node. Right-click the template and choose
+ * Open. You can then make changes to the template in the Source Editor.
+ */
+
+package runtime;
+
+/**
+ * A domain context represents the state of a particular instance of a LEM
+ * Domain.
+ *
+ * @author sjr
+ */
+public class DomainContext extends Context {
+    
+    /** Creates a new instance of DomainContext */
+    public DomainContext() {
+    }
+}
