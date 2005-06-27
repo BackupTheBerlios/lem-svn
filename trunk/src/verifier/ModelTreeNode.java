ee7a009a7964fd94e8f1ab19c35a4c1147f6a441
u4128551 <u4128551@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-06-27 07:03:47 +0000

    Completed Javadoc for ModelTreeNode.java

--- trunk/src/verifier/ModelTreeNode.java
+++ trunk/src/verifier/ModelTreeNode.java
@@ -12,13 +12,18 @@ import metamodel.Domain;
 import metamodel.Model;
 
 /**
- *
+ * Tree node appearing inside a ModelTreePanel. For graphically representing and 
+ * "holding" LEM Model objects. Has DomainNode as a child.
  * @author sjr
  */
 public class ModelTreeNode extends AbstractDescriptionNode {
     Model model;
     
-    /** Creates a new instance of ModelTreeNode */
+    /** 
+    * Creates a new instance of ModelTreeNode. Creates DomainNode branches 
+    * based on LEM Model object.
+    * @param m the LEM Model object.
+    */
     public ModelTreeNode( Model m ) {
         this.model = m;
         Iterator i = m.getDomains().values().iterator();
@@ -27,14 +32,24 @@ public class ModelTreeNode extends AbstractDescriptionNode {
             add( new DomainNode( (Domain)i.next() ));
         }
     }
- 
+    /**
+    * Returns name property of LEM Model object.
+    * @return the Model name.
+    */ 
     public String toString() {
         return "Model " + model.getName();
     }
-    
+    /**
+    * Returns the description property of the LEM Model object.
+    * @return the Model description.
+    */    
     public String getDescription(){
         return trim(model.getDescription());
     }
+    /**
+    * Returns the ContextMenu based on the LEM Model object.
+    * @return the Model ContextMenu.
+    */
     public JPopupMenu getContextMenu()
     {
         JPopupMenu ContextMenu = new JPopupMenu();
