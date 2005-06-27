83a1aa8100628c752f1b995c006f30855b4e881b
u4128551 <u4128551@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-06-27 07:33:53 +0000

    Completed Javadoc for StateNode.

--- trunk/src/verifier/StateNode.java
+++ trunk/src/verifier/StateNode.java
@@ -10,33 +10,44 @@ import javax.swing.JPopupMenu;
 import metamodel.State;
 
 /**
- *
+ * Tree node appearing inside a StateMachineNode. For graphically representing 
+ * and "holding" a LEM State object. Has no children.
  * @author SHukuBOy
  */
 public class StateNode extends AbstractDescriptionNode {
     
     State state ; 
-    /** Creates a new instance of StateNode */
+    /** 
+     * Creates a new instance of StateNode.
+     * @param state LEM State object to be contained.
+     */
     public StateNode( State state ) {
         this.state = state ; 
     }
-    
+    /**
+    * Returns name property of LEM State object.
+    * @return the State name.
+    */       
     public String toString()  {
         return "State : " + state.getName() ; 
     }
-    
+    /**
+    * Returns the description property of the LEM State object.
+    * @return the State description.
+    */ 
     public String getDescription()  {
         if (state.getDescription() != null)
             return trim(state.getDescription()) ; 
         else 
             return "" ; 
     }
-    
+    /**
+    * Returns the ContextMenu based on the LEM State object.
+    * @return the State ContextMenu.
+    */
     public JPopupMenu getContextMenu()
     {
         JPopupMenu ContextMenu = new JPopupMenu();
         return ContextMenu;
-    }
-    
-    
+    }       
 }
