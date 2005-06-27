b14443f49c03411737b91db254ce1eba024261fd
u4128551 <u4128551@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-06-27 04:42:51 +0000

    Added javadoc for attribute.

--- trunk/src/verifier/AttributeNode.java
+++ trunk/src/verifier/AttributeNode.java
@@ -10,7 +10,7 @@ import javax.swing.JPopupMenu;
 import metamodel.Attribute;
 
 /**
- *
+ * Tree node for representing and "holding" LEM attribute objects. Has no children.
  * @author sjr
  */
 public class AttributeNode extends AbstractDescriptionNode {
@@ -21,17 +21,23 @@ public class AttributeNode extends AbstractDescriptionNode {
     public AttributeNode( Attribute a ) {
         this.attribute = a;
     }
-    
+    /** Returns the name of the attribute followed by the type of the attribute */
     public String toString() {
         return attribute.getName() + " : " + attribute.getType().getName();
     }
- 
+    /** 
+     * Returns the description of the attribute, will return an empty string if
+     * description is null.
+     */
     public String getDescription() {        
         if (attribute.getDescription() != null)
             return trim(attribute.getDescription() );        
         else
             return "" ;                         
     }
+    /**
+     * Creates and returns a JPopupMenu based on the specified attribute.
+     */
     public JPopupMenu getContextMenu()
     {
         JPopupMenu ContextMenu = new JPopupMenu();
