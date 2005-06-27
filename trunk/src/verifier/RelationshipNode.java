b2de92fd65cbce5a298e5c3cb8fb023b22aa7ee5
u4128551 <u4128551@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-06-27 07:13:02 +0000

    Completed Javadoc for RelationshipNode.

--- trunk/src/verifier/RelationshipNode.java
+++ trunk/src/verifier/RelationshipNode.java
@@ -10,22 +10,32 @@ import javax.swing.JPopupMenu;
 import metamodel.Relationship;
 
 /**
- *
+ * Tree node appearing inside a DomainNode. For graphically representing and 
+ * "holding" a LEM Relationship object. Has no children.
  * @author sjr
  */
 public class RelationshipNode extends AbstractDescriptionNode{
     
     Relationship relationship = null;
     
-    /** Creates a new instance of RelationshipNode */
+    /**
+     * Creates a new instance of RelationshipNode. 
+     * @param r LEM Relationship object to be contained.
+     */
     public RelationshipNode( metamodel.Relationship r ) {
         this.relationship = r;
     }
-    
+    /**
+    * Returns name property of LEM Relationship object.
+    * @return the Relationship name.
+    */    
     public String toString() {
         return "Relationship " + relationship.getName(); 
     }
-    
+        /**
+    * Returns the description property of the LEM Relationship object.
+    * @return the Relationship description.
+    */
     public String getDescription()
     {
         if (relationship.getDescription() != null)
@@ -33,7 +43,10 @@ public class RelationshipNode extends AbstractDescriptionNode{
         else 
             return "" ; 
     }
-    
+    /**
+    * Returns the ContextMenu based on the LEM Relationship object.
+    * @return the Relationship ContextMenu.
+    */    
     public JPopupMenu getContextMenu()
     {
         JPopupMenu ContextMenu = new JPopupMenu();
