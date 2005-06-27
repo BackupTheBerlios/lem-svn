a9630fce79eec6cf2357531aeca806209567e7d1
u4128551 <u4128551@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-06-27 07:45:15 +0000

    Completed Javadoc for TransitionNode.

--- trunk/src/verifier/TransitionNode.java
+++ trunk/src/verifier/TransitionNode.java
@@ -10,7 +10,8 @@ import javax.swing.JPopupMenu;
 import metamodel.Transition;
 
 /**
- *
+ * Tree node appearing inside a StateMachineNode. For graphically representing 
+ * and "holding" a LEM Transition object. Has no children.
  * @author SHukuBOy
  */
 public class TransitionNode extends AbstractDescriptionNode {
@@ -18,23 +19,35 @@ public class TransitionNode extends AbstractDescriptionNode {
     Transition transition ; 
     String name ; 
     
-    /** Creates a new instance of TransitionNode */
+    /** Creates a new instance of TransitionNode.
+     * @param Transition LEM Transition object.
+     * @param name name of Transition object.
+     */
     public TransitionNode( Transition Transition , String name ) {
         this.transition = Transition ; 
         this.name = name ; 
     }
-    
+    /**
+    * Returns name property of LEM Transition object.
+    * @return the Transition name.
+    */ 
     public String toString()  {
         return name ; 
     }
-    
+    /**
+    * Returns the description property of the LEM Transition object.
+    * @return the Transition description.
+    */    
     public String getDescription()  { 
         if (transition.getDescription() != null)
             return trim(transition.getDescription()) ;
         else
             return "" ;
     }
-    
+    /**
+    * Returns the ContextMenu based on the LEM Transition object.
+    * @return the Transition ContextMenu.
+    */    
         public JPopupMenu getContextMenu()
     {
         JPopupMenu ContextMenu = new JPopupMenu();
