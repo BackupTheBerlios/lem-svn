375ef163ff2ccf216350310c373a8b05c9949805
u3958283 <u3958283@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-06-29 08:36:12 +0000

    javadoc fixed.

--- trunk/src/verifier/Cmdline.java
+++ trunk/src/verifier/Cmdline.java
@@ -36,40 +36,46 @@ public class Cmdline {
     public Cmdline(String file) {
         
         try {
-		File selectedFile = new File(file);
-        	Model m = loadModel( selectedFile );
-                if( m != null )
-                    models.add( m );
+            File selectedFile = new File(file);
+            Model m = loadModel( selectedFile );
+            if( m != null )
+                models.add( m );
         } catch (FileNotFoundException fnfe) {
-		System.out.println("File not found");
-	} catch( ParseException pe ) {
-                System.out.println("Parse Error: " + pe.getMessage());
+            System.out.println("File not found");
+        } catch( ParseException pe ) {
+            System.out.println("Parse Error: " + pe.getMessage());
         } catch( LemException le ) {
-                System.out.println("Parse Error: " + le.getMessage());
+            System.out.println("Parse Error: " + le.getMessage());
         } catch( IOException ioe ) {
-                System.out.println("I/OO Error: " + ioe.getMessage());
+            System.out.println("I/OO Error: " + ioe.getMessage());
         }
         
 /*        metamodel.Procedure p = m.getDomain( "Publications" ).getClass( "Manuscript" ).getStateMachine().getState("Adding").getProcedure();
         runtime.ModelInstance i = new runtime.ModelInstance();
-        
+ 
         p.execute(i); */
     }
-
+    
+    /*This method loads a LEM model specified by the parameter, and returns true
+     *if model loaded succesfully and false otherwise
+     *@param modelFile name of the LEM model file.
+     *@return true if model loaded succesfully and false otherweise
+     */
+    
     public Model loadModel( File modelFile ) throws FileNotFoundException, ParseException, LemException, IOException {
         Lem l = new Lem();
         FileInputStream fis = new FileInputStream( modelFile );
         
         return l.parse( fis );
     }
-
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-	if (args.length != 1)
-		System.out.println("Incorrect usage");
-	else
-		new Cmdline(args[0]);
+        if (args.length != 1)
+            System.out.println("Incorrect usage");
+        else
+            new Cmdline(args[0]);
     }
 }
