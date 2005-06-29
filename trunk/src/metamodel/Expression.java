e5958ef6348d721879cc135b15991f145455c240
u3957053 <u3957053@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-06-29 23:02:06 +0000

    Added Expression

---
+++ trunk/src/metamodel/Expression.java
@@ -0,0 +1,28 @@
+
+package metamodel;
+
+/**
+ * Represents a LEM expression
+ *
+ * @author sjr
+ */
+public abstract class Expression {
+    
+    /**
+     * This expression's type
+     *
+     * @todo Is it possible to determine this type at compile time? If not,
+     * this variable is probably inappropriate
+     */
+    
+//    DataType type;
+    
+    /** Creates a new instance of Expression */
+    public Expression() {
+    }
+    
+    /**
+     * Gets this expression's type
+     */
+//    public abstract DataType getType();
+}
