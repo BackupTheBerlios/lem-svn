e2fa10aec2fc34e821516a69a4d76ac98f54c8ad
u3957053 <u3957053@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-06-30 15:36:36 +0000

    Add a dummy evaluateExpression method, reformat code

--- trunk/src/runtime/Interpreter.java
+++ trunk/src/runtime/Interpreter.java
@@ -3,6 +3,7 @@
  */
 
 package runtime;
+import java.math.BigDecimal;
 import metamodel.*;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -15,43 +16,68 @@ public class Interpreter {
     /** Creates a new instance of Interpreter */
     public Interpreter() {
     }
-
+    
     public void interpret(Procedure p, Context c) throws LemRuntimeException {
-	    executeProcedure(p, c);
+        executeProcedure(p, c);
     }
     
-    public void executeProcedure ( Procedure p, Context c ) throws LemRuntimeException {
-	LinkedList actions = p.getActions();
+    public void executeProcedure( Procedure p, Context c ) throws LemRuntimeException {
+        LinkedList actions = p.getActions();
         Iterator i = actions.iterator();
         
         while( i.hasNext() ) {
             Action a = (Action)i.next();
-	    executeAction(a, c);
+            executeAction(a, c);
         }
     }
-
-    public void executeAction ( Action a, Context c ) throws LemRuntimeException {
-	if ( a instanceof CreateAction )
-		executeCreateAction((CreateAction)a, c);
-	else {
-		throw new LemRuntimeException ("executeAction encountered unknown action");
-	}
+    
+    public void executeAction( Action a, Context c ) throws LemRuntimeException {
+        if ( a instanceof CreateAction )
+            executeCreateAction((CreateAction)a, c);
+        else {
+            throw new LemRuntimeException("executeAction encountered unknown action");
+        }
     }
-
+    
     public runtime.Object executeCreateAction( CreateAction a, Context c ) throws LemRuntimeException {
-	// Create the new object
+        // Create the new object
         runtime.Object o = new runtime.Object( a.getClasses() );
-	    
+        
         // Add it to the context
         c.addObject(o);
-	    
+        
         // Notify listeners that the object has been added
         LemObjectCreationEvent e = new LemObjectCreationEvent( o, a );
         for( Iterator i = c.getLemEventListeners().iterator(); i.hasNext(); ) {
-        	LemEventListener l = (LemEventListener)i.next();
-                l.objectCreated(e);
-         }
-	    
-	 return o;
+            LemEventListener l = (LemEventListener)i.next();
+            l.objectCreated(e);
+        }
+        
+        return o;
+    }
+    
+    public Variable evaluateExpression( Expression e, Context c ) throws LemRuntimeException {
+        if( e instanceof Literal ) {
+            Literal l = (Literal)e;
+            
+            // TODO: Testing only
+            Variable v = new Variable( l.getType() );
+            v.setValue( new BigDecimal( l.getValue() ));
+            
+            return v;
+        }
+        
+        if( e instanceof BinaryOperation ) {
+            BinaryOperation o = (BinaryOperation) e;
+            Variable left = evaluateExpression( o.getLeft(), c );
+            Variable right = evaluateExpression( o.getRight(), c );
+            
+            Variable v = new Variable( left.getType() );
+            v.setValue( ((BigDecimal)left.getValue()).add((BigDecimal)right.getValue()));
+    
+            return v;
+        }
+        
+        return null;
     }
 }
