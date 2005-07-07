25888effadd68cbdef193917f5d9a7dfd7a09f5d
u3957053 <u3957053@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-07-07 23:18:39 +0000

    Add piles of javadoc, get rid of setValue( BigDecimal ) method

--- trunk/src/runtime/NumericVariable.java
+++ trunk/src/runtime/NumericVariable.java
@@ -1,42 +1,102 @@
-/*
- * NumericVariable.java
- *
- * Created on July 6, 2005, 5:43 PM
- *
- * To change this template, choose Tools | Options and locate the template under
- * the Source Creation and Management node. Right-click the template and choose
- * Open. You can then make changes to the template in the Source Editor.
- */
 
 package runtime;
 
 import java.math.BigDecimal;
 import metamodel.CoreDataType;
+import metamodel.DataType;
 import metamodel.NumericType;
 import metamodel.StringType;
 
 /**
- *
+ * This class represents instances of LEM NumericTypes. Operations on this NumericVariable
+ * should respect any constraints imposed by the variable's DataType. For example, if the
+ * DataType is a DomainSpecificDataType, then precision and range restrictions may be imposed.
+ * 
+ * NumericTypes, like other instances of Variable, are (usually) immutable. That is, operations
+ * such as add() and subtract() will result in the creation of new Variable instances.
  * @author sjr
  */
 public class NumericVariable extends Variable {
     
     BigDecimal value;
     
-    /** Creates a new instance of NumericVariable */
+    /**
+     * Creates a new instance of NumericVariable.
+     * @param value the String representing the value of this numeric variable
+     * @throws runtime.LemRuntimeException if <code>value</code> does not represent a valid Java BigDecimal when passed to BigDecimal's constructor
+     * @see API for java.math.BigDecimal
+     */
     public NumericVariable( String value ) throws LemRuntimeException {
         try {
             setValue( new BigDecimal( value ));
+            setType( NumericType.getInstance() );
         } catch( NumberFormatException nfe ) {
             throw new LemRuntimeException( "NumberFormatException while creating new NumericVariable" );
         }
     }
     
+    /**
+     * Create a new instance of NumericVariable using the given BigDecimal as a value.
+     * @param value the BigDecimal to be used as a value
+     */
     public NumericVariable( BigDecimal value ) {
         this.value = value;
         setType( NumericType.getInstance() );
     }
     
+    /**
+     * Create a new NumericVariable with the given DataType. This type is usually
+     * (but not necessarily) a DomainSpecificDataType specifying constraints on the
+     * value of this NumericVariable.
+     * @param type the type of this variable
+     */
+    public NumericVariable( DataType type ) {
+        this.value = new BigDecimal( "0" );
+        setType( type );
+    }
+    
+    /**
+     * Create a new instance of NumericVariable with the given type and value.
+     * @param type the type, usually (but not necessarily) a DomainSpecificDataType
+     * @param value the value to be assumed by this new variable
+     */
+    public NumericVariable( DataType type, BigDecimal value ) {
+        this.value = value;
+        setType( type );
+    }
+    
+    /**
+     * Create a new instance of NumericVariable with the given type and value.
+     * @param type the type, usually (but not necessarily) a DomainSpecificDataType
+     * @param value the value to be assumed by this variable
+     * @throws runtime.LemRuntimeException if the value String does not represent a valid Java BigDecimal
+     * @see the java.math.BigDecimal API
+     */
+    public NumericVariable( DataType type, String value ) throws LemRuntimeException {
+        try {
+            this.value = new BigDecimal( value );
+            setType( type );
+        } catch( NumberFormatException nfe ) {
+            System.err.println( "Invalid string passed to NumericVariable constructor: " + nfe.getMessage() );
+        }
+    }
+    
+    /**
+     * Adds this NumericVariable to the given Variable. If <code>right</code> is a 
+     * StringType, then return a new StringVariable which is represents the String
+     * given by the Java statement:
+     * 
+     * <code>value.toString() + (String)right.getValue()</code>
+     * 
+     * That is, the String representation of the BigDecimal <code>value</code> is prepended
+     * to the String of the StringVariable on the right.
+     * 
+     * If <code>right</code> is a NumericType, then return a new NumericVariable 
+     * representing the BigDecimal sum of the two variables.
+     * @param right the variable to add
+     * @throws runtime.LemRuntimeException if <code>right</code> is not a NumericType or StringType
+     * @return a new Variable
+     */
     public Variable add( Variable right ) throws LemRuntimeException {
         CoreDataType rightType = right.getCoreDataType();
         if( StringType.getInstance().equals( rightType )) {
@@ -49,6 +109,13 @@ public class NumericVariable extends Variable {
         throw new LemRuntimeException( "Operation add(numeric, " + right.getType().getName() + "' not supported" );
     }
 
+    /**
+     * Returns a new variable representing the difference between <code>value</code> and the
+     * BigDecimal value of the <code>right</code> NumericType.
+     * @param right the variable to subtract
+     * @throws runtime.LemRuntimeException if <code>right</code> is not a NumericType
+     * @return a new NumericVariable representing the difference between this variable and <code>right</code>
+     */
     public Variable subtract( Variable right ) throws LemRuntimeException {
         CoreDataType rightType = right.getCoreDataType();
         if( StringType.getInstance().equals( rightType )) {
@@ -61,6 +128,13 @@ public class NumericVariable extends Variable {
         throw new LemRuntimeException( "Operation subtract(numeric, " + right.getType().getName() + "' not supported" );
     }
     
+    /**
+     * Returns a new NumericVariable representing the product of this NumericVariable and
+     * the NumericVariable <code>right</code>.
+     * @param right the variable with which to multiply
+     * @throws runtime.LemRuntimeException if <code>right</code> is not a NumericType
+     * @return a new NumericVariable representing the BigDecimal product of this and <code>right</code>
+     */
     public Variable multiply( Variable right ) throws LemRuntimeException {
         if( NumericType.getInstance().equals( right.getCoreDataType() )) {
             return new NumericVariable( value.multiply( (BigDecimal)right.getValue() ));
@@ -69,6 +143,13 @@ public class NumericVariable extends Variable {
         throw new LemRuntimeException( "Operation multiply(numeric, " + right.getType().getName() + "' not supported" );
     }
     
+    /**
+     * Returns a new NumericVariable representing the quotient of this NumericVariable and
+     * the NumericVariable <code>right</code>.
+     * @param right the variable by which to divide
+     * @throws runtime.LemRuntimeException if <code>right</code> is not a NumericType
+     * @return a new NumericVariable representing the BigDecimal quotient of this and <code>right</code>
+     */
     public Variable divide( Variable right ) throws LemRuntimeException {
         if( NumericType.getInstance().equals( right.getCoreDataType() )) {
             try {
@@ -81,10 +162,27 @@ public class NumericVariable extends Variable {
         throw new LemRuntimeException( "Operation divide(numeric, " + right.getType().getName() + "' not supported" );
     }
     
+    /**
+     * Returns equal( b ).logicalNot().
+     *
+     * @param b the Variable with which to compare
+     * @throws runtime.LemRuntimeException if the call to equal() generates an exception
+     * @return the BooleanVariable representing the truth of this comparison
+     * @see NumericVariable#equal( Variable )
+     */
     public Variable notEqual(Variable b) throws LemRuntimeException {
         return equal( b ).logicalNot();
     }
     
+    /**
+     * Returns the remainder of dividing this variable by the NumericVariable <code>b</code>.
+     *
+     * @param b the NumericVariable by which to divide
+     * @throws runtime.LemRuntimeException if <code>b</code> is not a NumericVariable
+     * @return a new NumericVariable which contains the result of the BigDecimal.remainder()
+     * operation applied to this and <code>b</code>
+     * @see the java.math.BigDecimal remainder() API call
+     */
     public Variable mod(Variable b) throws LemRuntimeException {
         if( !NumericType.getInstance().equals( b.getCoreDataType() ))
             throw new LemRuntimeException( "Operation mod(numeric, " + b.getType().getName() + "' not supported" );
@@ -93,6 +191,13 @@ public class NumericVariable extends Variable {
         return new NumericVariable( value.remainder( right ));
     }
     
+    /**
+     * Apply the BigDecimal.compareTo() method to this and <code>v</code>.
+     *
+     * @param v the NumericVariable with which to compare
+     * @throws runtime.LemRuntimeException if <code>v</code> is not a NumericVariable
+     * @return the result of the call to compareTo()
+     */
     private int compare( Variable v ) throws LemRuntimeException {
         if( !NumericType.getInstance().equals( v.getCoreDataType() ))
             throw new LemRuntimeException( "Operation compare(numeric, " + v.getType().getName() + "' not supported" );
@@ -102,6 +207,16 @@ public class NumericVariable extends Variable {
         return value.compareTo( right );
     }
 
+    /**
+     * Returns a new BooleanVariable representing whether this NumericValue
+     * represents the same value as the given NumericValue.
+     *
+     * <b>NOTE:</b> This method is subject to the oddities of the BigDecimal.compareTo() call.
+     * You are advised to be sure of its semantics.
+     * @param b the NumericVariable with which to compare 
+     * @throws runtime.LemRuntimeException if <code>b</code> is not a NumericVariable
+     * @return a new BooleanVariable representing the truth of the equality
+     */
     public Variable equal( Variable b ) throws LemRuntimeException {
         int cmp = compare( b );
         
@@ -110,7 +225,17 @@ public class NumericVariable extends Variable {
         else
             return new BooleanVariable( "false" );
     }
-    
+
+    /**
+     * Returns a new BooleanVariable representing whether this NumericValue
+     * represents the same a lesser or equal value when compared to the given NumericValue.
+     *
+     * <b>NOTE:</b> This method is subject to the oddities of the BigDecimal.compareTo() call.
+     * You are advised to be sure of its semantics.
+     * @param b the NumericVariable with which to compare 
+     * @throws runtime.LemRuntimeException if <code>b</code> is not a NumericVariable
+     * @return a new BooleanVariable representing the truth of the comparison
+     */
     public Variable lessThanOrEqual(Variable b) throws LemRuntimeException {
         int cmp = compare( b );
         
@@ -119,7 +244,17 @@ public class NumericVariable extends Variable {
         else
             return new BooleanVariable( "false" );
     }
-    
+
+    /**
+     * Returns a new BooleanVariable representing whether this NumericValue
+     * represents the same a lesser value when compared to the given NumericValue.
+     *
+     * <b>NOTE:</b> This method is subject to the oddities of the BigDecimal.compareTo() call.
+     * You are advised to be sure of its semantics.
+     * @param b the NumericVariable with which to compare 
+     * @throws runtime.LemRuntimeException if <code>b</code> is not a NumericVariable
+     * @return a new BooleanVariable representing the truth of the comparison
+     */
     public Variable lessThan(Variable b) throws LemRuntimeException {
         int cmp = compare( b );
         
@@ -129,6 +264,16 @@ public class NumericVariable extends Variable {
             return new BooleanVariable( "false" );
     }
     
+    /**
+     * Returns a new BooleanVariable representing whether this NumericValue
+     * represents the same an equal or greater value when compared to the given NumericValue.
+     *
+     * <b>NOTE:</b> This method is subject to the oddities of the BigDecimal.compareTo() call.
+     * You are advised to be sure of its semantics.
+     * @param b the NumericVariable with which to compare 
+     * @throws runtime.LemRuntimeException if <code>b</code> is not a NumericVariable
+     * @return a new BooleanVariable representing the truth of the comparison
+     */
     public Variable greaterThanOrEqual(Variable b) throws LemRuntimeException {
         int cmp = compare( b );
         
@@ -138,6 +283,16 @@ public class NumericVariable extends Variable {
             return new BooleanVariable( "false" );
     }
     
+    /**
+     * Returns a new BooleanVariable representing whether this NumericValue
+     * represents a greater value when compared to the given NumericValue.
+     *
+     * <b>NOTE:</b> This method is subject to the oddities of the BigDecimal.compareTo() call.
+     * You are advised to be sure of its semantics.
+     * @param b the NumericVariable with which to compare 
+     * @throws runtime.LemRuntimeException if <code>b</code> is not a NumericVariable
+     * @return a new BooleanVariable representing the truth of the comparison
+     */
     public Variable greaterThan(Variable b) throws LemRuntimeException {
         int cmp = compare( b );
         
@@ -147,18 +302,31 @@ public class NumericVariable extends Variable {
             return new BooleanVariable( "false" );
     }
     
+    /**
+     * Returns a new NumericVariable representing the negation (multiplication by -1) of this NumericVariable
+     * @throws runtime.LemRuntimeException 
+     * @return a new NumericVariable representing the negation of this NumericVariable
+     */
     public Variable negation() throws LemRuntimeException {
         return new NumericVariable( value.negate() );
     }
-    
-    public void setValue( BigDecimal value ) {
-        this.value = value;
-    }
-    
+        
+    /**
+     * Sets the value of this NumericVariable to the given java.lang.Object. The 
+     * object is unconditionally cast to a BigDecimal, so it is up to the caller to 
+     * ensure that the given Object is a BigDecimal.
+     *
+     * @param o the new BigDecimal value
+     */
     public void setValue( java.lang.Object o ) {
         this.value = (BigDecimal)o;
     }
     
+    /**
+     * Returns the BigDecimal value as a java.lang.Object.
+     *
+     * @return the value of this NumericVariable
+     */
     public java.lang.Object getValue() {
         return value;
     }
