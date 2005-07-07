9787d2a80b3602401c33012ef5fdde6b2e706917
u3957053 <u3957053@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-07-07 10:57:27 +0000

    Added evalTest

--- trunk/src/test/runtime/InterpreterTest.java
+++ trunk/src/test/runtime/InterpreterTest.java
@@ -14,6 +14,7 @@ import java.math.BigDecimal;
 import metamodel.AssignmentAction;
 import metamodel.LemException;
 import metamodel.Model;
+import metamodel.Procedure;
 import parser.ParseException;
 import tools.Lem;
 
@@ -59,10 +60,42 @@ public class InterpreterTest extends junit.framework.TestCase {
             Variable v = i.executeAssignmentAction(a, c);
             System.out.println( "The answer is " + v.getValue() );
             BigDecimal val = (BigDecimal)v.getValue();
-            assertEquals( "The result is 2.1 + 3.2 + 4.3 = 9.6", true, val.equals( new BigDecimal( "9.6" )));
+            assertEquals( "The result is 2.1 + 3.2 + 4.3 = 9.6", true, v.getValue().equals( new BigDecimal( "9.6" )));
         } catch( LemRuntimeException e ) {
             fail( "Runtime exception while executing test: " + e.getMessage() );
         }
         
+    }
+    
+    public void testEvaluation() {
+        Lem l = new Lem();
+        Model m = null;
+        
+        try {
+            m = l.parse( new FileInputStream( "regression/tests/AssociativityTest.lem" ));
+        } catch( FileNotFoundException fnfe ) {
+            fail( "Could not find model file " + fnfe.getMessage() );
+        } catch( IOException e ) {
+            fail( "Could not read model file: " + e.getMessage() );
+        } catch( ParseException e ) {
+            fail( "Could not parse model file: " + e.getMessage() );
+        } catch( LemException e ) {
+            fail( "Some LEMException occurred: " + e.getMessage() );
+        }
+        
+        Procedure p = m.getDomain("TestDomain")
+            .getClass("A")
+            .getStateMachine()
+            .getState("evalTest")
+            .getProcedure();
+        
+        DomainContext c = new DomainContext();
+        Interpreter i = new Interpreter();
+        
+        try {
+            i.executeProcedure( p, c );
+        } catch( LemRuntimeException e ) {
+            fail( "Some LemRuntimeException occurred: " + e.getMessage() );
+        }
     }
 }
