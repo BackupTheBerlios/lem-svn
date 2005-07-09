f1b333983fd2121433de334119028b7e167fbd5e
u3956099 <u3956099@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-07-09 11:56:11 +0000

    All required controls should now be present

--- branches/toshio_diagen/src/verifier/GenerateStatechartDialog.java
+++ branches/toshio_diagen/src/verifier/GenerateStatechartDialog.java
@@ -6,6 +6,11 @@
 
 package verifier;
 
+import java.awt.Component;
+import java.awt.Frame;
+import java.util.Collection;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author  gen
@@ -13,9 +18,19 @@ package verifier;
 public class GenerateStatechartDialog extends javax.swing.JDialog {
 	
 	/**
-	 * Creates new form GenerateStatechartDialog 
+	 * Handle to the displayed dialog.
 	 */
-	public GenerateStatechartDialog(java.awt.Frame parent, boolean modal) {
+	private static GenerateStatechartDialog dialog = null;
+	
+	/**
+	 * Creates new form GenerateStatechartDialog 
+	 *
+	 * @param the dialog's parent window
+	 * @param modal whether dialog should be modal
+	 * @param statechartList the list of statecharts the dialog should display
+	 */
+	private GenerateStatechartDialog(java.awt.Frame parent, boolean modal, 
+			Collection statechartList) {
 		super(parent, modal);
 		initComponents();
 	}
@@ -27,26 +42,109 @@ public class GenerateStatechartDialog extends javax.swing.JDialog {
 	 */
   // <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:initComponents
   private void initComponents() {
-    gridLayout1 = new java.awt.GridLayout();
-    jLabel1 = new javax.swing.JLabel();
-    jTextField1 = new javax.swing.JTextField();
+    java.awt.GridBagConstraints gridBagConstraints;
+
+    statechartLabel = new javax.swing.JLabel();
+    statechartList = new javax.swing.JComboBox();
+    jLabel4 = new javax.swing.JLabel();
+    browseButton = new javax.swing.JButton();
+    destField = new javax.swing.JTextField();
+    generateButton = new javax.swing.JButton();
+    cancelButton = new javax.swing.JButton();
+
+    getContentPane().setLayout(new java.awt.GridBagLayout());
 
     setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+    setTitle("Generate Statechart");
     setName("Generate Statechart");
-    jLabel1.setText("Test label:");
-    getContentPane().add(jLabel1, java.awt.BorderLayout.CENTER);
+    addWindowListener(new java.awt.event.WindowAdapter() {
+      public void windowOpened(java.awt.event.WindowEvent evt) {
+        formWindowOpened(evt);
+      }
+    });
 
-    jTextField1.setText("jTextField1");
-    getContentPane().add(jTextField1, java.awt.BorderLayout.EAST);
+    statechartLabel.setText("Statechart:");
+    gridBagConstraints = new java.awt.GridBagConstraints();
+    gridBagConstraints.gridx = 0;
+    gridBagConstraints.gridy = 0;
+    gridBagConstraints.anchor = java.awt.GridBagConstraints.FIRST_LINE_START;
+    getContentPane().add(statechartLabel, gridBagConstraints);
+
+    gridBagConstraints = new java.awt.GridBagConstraints();
+    gridBagConstraints.gridx = 1;
+    gridBagConstraints.gridy = 0;
+    gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+    gridBagConstraints.anchor = java.awt.GridBagConstraints.FIRST_LINE_START;
+    getContentPane().add(statechartList, gridBagConstraints);
+
+    jLabel4.setText("Destination:");
+    gridBagConstraints = new java.awt.GridBagConstraints();
+    gridBagConstraints.gridx = 0;
+    gridBagConstraints.gridy = 1;
+    gridBagConstraints.anchor = java.awt.GridBagConstraints.FIRST_LINE_START;
+    getContentPane().add(jLabel4, gridBagConstraints);
+
+    browseButton.setMnemonic('B');
+    browseButton.setText("Browse...");
+    gridBagConstraints = new java.awt.GridBagConstraints();
+    gridBagConstraints.gridx = 2;
+    gridBagConstraints.gridy = 1;
+    gridBagConstraints.anchor = java.awt.GridBagConstraints.FIRST_LINE_START;
+    getContentPane().add(browseButton, gridBagConstraints);
+
+    destField.setText("jTextField1");
+    gridBagConstraints = new java.awt.GridBagConstraints();
+    gridBagConstraints.gridx = 1;
+    gridBagConstraints.gridy = 1;
+    gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+    gridBagConstraints.anchor = java.awt.GridBagConstraints.FIRST_LINE_START;
+    gridBagConstraints.weightx = 1.0;
+    getContentPane().add(destField, gridBagConstraints);
+
+    generateButton.setMnemonic('G');
+    generateButton.setText("Generate");
+    gridBagConstraints = new java.awt.GridBagConstraints();
+    gridBagConstraints.gridx = 0;
+    gridBagConstraints.gridy = 2;
+    getContentPane().add(generateButton, gridBagConstraints);
+
+    cancelButton.setMnemonic('C');
+    cancelButton.setText("Cancel");
+    gridBagConstraints = new java.awt.GridBagConstraints();
+    gridBagConstraints.gridx = 1;
+    gridBagConstraints.gridy = 2;
+    getContentPane().add(cancelButton, gridBagConstraints);
 
     pack();
   }
   // </editor-fold>//GEN-END:initComponents
+
+	private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
+		// Show the statechart list
+		
+	}//GEN-LAST:event_formWindowOpened
+	
+	/**
+	 * Display the dialog
+	 *
+	 * @param parent the component that this dialog belongs do
+	 * @param statechartList the list of statecharts the dialog should display
+	 */
+	public static String showDialog(Component parent, Collection statechartList) {
+		Frame frame = JOptionPane.getFrameForComponent( parent );
+		//dialog = new GenerateStatechartDialog( frame, true );
+		//dialog.setVisible( true );
+		return "This is a test";
+	}
 	
   // Variables declaration - do not modify//GEN-BEGIN:variables
-  private java.awt.GridLayout gridLayout1;
-  private javax.swing.JLabel jLabel1;
-  private javax.swing.JTextField jTextField1;
+  private javax.swing.JButton browseButton;
+  private javax.swing.JButton cancelButton;
+  private javax.swing.JTextField destField;
+  private javax.swing.JButton generateButton;
+  private javax.swing.JLabel jLabel4;
+  private javax.swing.JLabel statechartLabel;
+  private javax.swing.JComboBox statechartList;
   // End of variables declaration//GEN-END:variables
 	
 }
