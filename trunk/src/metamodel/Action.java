f118d7fa1b52442a11fd671fc6d756df443c0666
u3957053 <u3957053@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-07-10 02:12:49 +0000

    Remove silly NetBeans boilerplate

--- trunk/src/metamodel/Action.java
+++ trunk/src/metamodel/Action.java
@@ -19,16 +19,6 @@
  * USA. 
  */
 
-/*
- * Action.java
- *
- * Created on May 14, 2005, 9:56 AM
- *
- * To change this template, choose Tools | Options and locate the template under
- * the Source Creation and Management node. Right-click the template and choose
- * Open. You can then make changes to the template in the Source Editor.
- */
-
 package metamodel;
 
 /**
