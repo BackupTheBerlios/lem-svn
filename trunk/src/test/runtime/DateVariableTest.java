cf7e39da7cc643a877d9b2fc9b6759347695ccb0
u3958283 <u3958283@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-07-14 15:35:52 +0000

    Used to test DateVariable.java, not much into it yet, has to be completed.

---
+++ trunk/src/test/runtime/DateVariableTest.java
@@ -0,0 +1,43 @@
+/*
+ * DateVariableTest.java
+ * JUnit based test
+ *
+ * Created on 14 July 2005, 22:34
+ */
+
+package test.runtime;
+
+import java.util.Date;
+import runtime.DateVariable ;
+import runtime.LemRuntimeException ;
+/**
+ *
+ * @author shuku
+ */
+public class DateVariableTest extends junit.framework.TestCase {
+    
+    public DateVariableTest(String testName) {
+        super(testName);       
+    }
+    
+    
+    public void testGetType() {
+        DateVariable d = new DateVariable() ; 
+        System.out.println( d.getType().getName())  ; 
+    }
+    
+    public void testGetValue() {        
+        DateVariable d = null ;
+        try {
+            d = new DateVariable("10-12-2030") ;
+            Date date = d.getDate() ;
+            String theDate = (String) d.getValue() ;
+            System.err.println(theDate) ; 
+            assertEquals( "DateVariable created and converted correctly", true, theDate.equals("10-12-2030") );
+        } catch( LemRuntimeException e ) {
+            fail("Failed Because :" + e.getMessage() );
+        }
+    }          
+    
+    
+}
