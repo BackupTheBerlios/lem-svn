85cdd9d957f04019f5603950e08226c9c5510fca
u3957053 <u3957053@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-07-15 05:50:12 +0000

    Reformat code, fix capitalisation of variable names

--- trunk/src/verifier/StateNode.java
+++ trunk/src/verifier/StateNode.java
@@ -14,66 +14,62 @@ import runtime.Interpreter;
 import runtime.LemRuntimeException;
 
 /**
- * Tree node appearing inside a StateMachineNode. For graphically representing 
+ * Tree node appearing inside a StateMachineNode. For graphically representing
  * and "holding" a LEM State object. Has no children.
  * @author SHukuBOy
  */
 public class StateNode extends AbstractDescriptionNode {
     /**The LEM State object that StateNode contains.*/
-    State state ; 
-    /** 
+    State state ;
+    /**
      * Creates a new instance of StateNode.
      * @param state LEM State object to be contained.
      */
     public StateNode( State state ) {
-        this.state = state ; 
+        this.state = state ;
     }
     /**
-    * Returns name property of LEM State object.
-    * @return the State name.
-    */       
+     * Returns name property of LEM State object.
+     * @return the State name.
+     */
     public String toString()  {
-        return "State : " + state.getName() ; 
+        return "State : " + state.getName() ;
     }
     /**
-    * Returns the description property of the LEM State object.
-    * @return the State description.
-    */ 
+     * Returns the description property of the LEM State object.
+     * @return the State description.
+     */
     public String getDescription()  {
         if (state.getDescription() != null)
-            return trim(state.getDescription()) ; 
-        else 
-            return "" ; 
+            return trim(state.getDescription()) ;
+        else
+            return "" ;
     }
     /**
-    * Returns the ContextMenu based on the LEM State object.
-    * @return the State ContextMenu.
-    */
-    public JPopupMenu getContextMenu()
-    {
-        JPopupMenu ContextMenu = new JPopupMenu();
+     * Returns the ContextMenu based on the LEM State object.
+     * @return the State ContextMenu.
+     */
+    public JPopupMenu getContextMenu() {
+        JPopupMenu contextMenu = new JPopupMenu();
         JMenuItem executeProcedure = new JMenuItem();
         executeProcedure.setText("Execute");
         executeProcedure.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                ExecuteProcedure();
+                executeProcedure();
             }
         });
-        ContextMenu.add(executeProcedure);
-         return ContextMenu;
-    }       
+        contextMenu.add(executeProcedure);
+        return contextMenu;
+    }
     
-    public void ExecuteProcedure() 
-    {
+    public void executeProcedure() {
         metamodel.Procedure p = this.state.getProcedure();
         runtime.DomainContext d = new DomainContext();
         ConsoleLogger c = new ConsoleLogger(d);
-        runtime.Interpreter I = new Interpreter();
+        runtime.Interpreter i = new Interpreter();
         try{
-        I.interpret(p, d);
-        }
-        catch(LemRuntimeException e)
-        {
+            i.interpret(p, d);
+        } catch(LemRuntimeException e) {
             System.out.println(e);
         }
     }
