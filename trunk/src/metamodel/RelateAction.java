611cd348714401559e847c7813f3770232c98527
u3957053 <u3957053@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-07-17 07:13:30 +0000

    Add the class representing the relate action

---
+++ trunk/src/metamodel/RelateAction.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright (C) 2005 James Ring
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ */
+
+
+package metamodel;
+
+/**
+ * This class represents the "relate" action which, when executed, causes
+ * two instances to be associated in some way.
+ * @author sjr
+ */
+public class RelateAction {
+    
+    /**
+     * The name of the Variable containing a reference to the 
+     * 'active' object in the Association.
+     */
+    protected String activeObjectName = null;
+    
+    /**
+     * The name of the Variable containing a reference to the 
+     * 'passive' object in the Association.
+     */
+    protected String passiveObjectName = null;
+    
+    /**
+     * The name of the variable which will be assigned the object reference
+     * of any instantiated association class.
+     */
+    protected String associationClassReference = null;
+    
+    /** Creates a new instance of RelateAction */
+    public RelateAction() {
+    }
+
+    
+    /**
+     * Get the name of the Variable containing a reference to the active object.
+     *
+     * @return the Variable name
+     */
+    public String getActiveObjectName() {
+        return activeObjectName;
+    }
+
+    /**
+     * Set the name of the Variable containing a reference to the active object.
+     *
+     * @param activeObjectName the Variable name
+     */
+    public void setActiveObjectName(String activeObjectName) {
+        this.activeObjectName = activeObjectName;
+    }
+
+    /**
+     * Get the name of the Variable containing a reference to the passive object.
+     *
+     * @return the Variable name
+     */
+    public String getPassiveObjectName() {
+        return passiveObjectName;
+    }
+
+    /**
+     * Set the name of the Variable containing a reference to the passive object.
+     *
+     * @param passiveObjectName the Variable name
+     */
+    public void setPassiveObjectName(String passiveObjectName) {
+        this.passiveObjectName = passiveObjectName;
+    }
+
+    /**
+     * Returns the name of the variable which will be assigned the object reference
+     * of any instantiated association class.
+     *
+     * @return the Variable name
+     */
+    public String getAssociationClassReference() {
+        return associationClassReference;
+    }
+    
+    /**
+     * Set the name of the variable which will be assigned the object reference
+     * of any instantiated association class.
+     *
+     * @param associationClassReference the Variable name
+     */
+    public void setAssociationClassReference(String associationClassReference) {
+        this.associationClassReference = associationClassReference;
+    }
+    
+}
