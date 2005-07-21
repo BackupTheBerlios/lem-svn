36c238fa117d38b0bd7c2af66b0d9020698fcb58
u3956099 <u3956099@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-07-21 13:09:34 +0000

    Now substantially complete. Now uses ResourceBundle for default properties to eliminate issues with default file locations. This should mean that default properties can be accessed directly from the JAR file, eliminating the fragility of the previous file-dependent implementation.

--- trunk/src/verifier/Parameters.java
+++ trunk/src/verifier/Parameters.java
@@ -20,12 +20,15 @@
  */
 
 package verifier;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Enumeration;
 import java.util.Properties;
+import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
 
 /**
  * Manages eLEMinator's persistent properties.
@@ -38,67 +41,154 @@ import java.util.Properties;
  * loaded. If a user property exists that has the same name as a default
  * property, then the default property is overriden by the user property -
  * unless the default property is requested explicitly by calling
- * getDefaultProperty()
+ * getDefaultProperty().
+ *
+ * This class uses the Singleton pattern to ensure that at most one instance
+ * of the class can exist at a time.
  *
  * For consistency, all eLEMinator property names should be of form
  * 'eleminator.<propertyname>'.  For example, 'eleminator.version' or
- * 'eleminator.graphgen.dotlocation'.
+ * 'eleminator.diagram.dotPath'.
  *
  * @author Toshio Nakamura
  * @todo This should be implemented as a Singleton
  */
 public class Parameters {
 	
+	static Parameters currentParams = new Parameters();
+	
 	/**
-	 * The location of the default properties file, relative to the xtuml base
-	 * directory
+	 * The name of the default resource bundle
 	 */
-	private static final String defaultPropertiesFile =
-					"eleminator_default.properties";
+	String defaultBundleName = "eleminatorDefault";
+	
+	/**
+	 * The name of the resource bundle containing the user properties list
+	 */
+	String userListBundleName = "userPropertiesList";
 	
 	/**
 	 * The default properties list
 	 */
-	private Properties defaultProps = null;
+	Properties defaultProps = null;
 	
 	/**
 	 * The user properties list
 	 */
-	private Properties userProps = null;
+	Properties userProps = null;
 	
 	/**
-	 * Loads the default and user properties files
+	 * Loads the default and user properties files. If no user properties file exists,
+	 * a new default one is created. Note that the class constructor is private
+	 * to prevent arbitrary instantiation, following the Singleton pattern.
 	 * @todo currently a test stub only
 	 * @todo implement user properties
+	 * @todo remove debugging code
+	 * @todo break into two separate procedures?
 	 */
-	public Parameters() {
-		// Load the default properties
+	private Parameters() {
+		
+		// Initialise default properties
+		
+		PropertyResourceBundle defaultBundle =
+				(PropertyResourceBundle) ResourceBundle.getBundle( defaultBundleName );
+		
+		// Convert the ResourceBundle into a Properties set. This allows the default
+		// properties to be easily set as a 'fallback' for the user properties, as
+		// described in the class header comment.
+		Enumeration keyList = defaultBundle.getKeys();
+		String currentKey = null;
 		defaultProps = new Properties();
-		FileInputStream in = null;
-		try {
-			in = new FileInputStream( defaultPropertiesFile );
-			defaultProps.load( in );
-		} catch (FileNotFoundException fnfe) {
-			System.err.println( "Error: Could not find default properties file" );
-			fnfe.printStackTrace();
-		} catch (IOException ioe ) {
-			System.err.println( "Error: Unable to load default properties file" );
-			ioe.printStackTrace();
+		while (keyList.hasMoreElements()) {
+			currentKey = (String)keyList.nextElement();
+			defaultProps.setProperty( currentKey, defaultBundle.getString( currentKey ) );
+		}
+		
+		// Initialise user properties
+		
+		// Get the user's home directory
+		String baseDir = System.getProperty( "user.home" );
+		
+		// Get the path to the properties file (operating system-dependent)
+		
+		String osName = System.getProperty( "os.name" );
+		// Two separate variables are required here due to the separate mkdir() and
+		// createNewFile() calls that are required to create a new file and its
+		// containing directories
+		String userPropsDir = null; // Directory containing the properties file
+		String userPropsPath = null; // Full path, including filename
+		if (osName.equals( "Linux" )) {
+			// Append a trailing path separator charactor, if necessary
+			if (baseDir.charAt( baseDir.length() - 1) != '/') {
+				baseDir = baseDir + "/";
+			}
+			userPropsDir = baseDir + this.getDefaultProperty( "eleminator.userPropertiesPath.linux" );
+		} else if (osName.equals( "Windows" )) {
+			// Append a trailing path separator charactor, if necessary
+			if (baseDir.charAt( baseDir.length() - 1) != '\\') {
+				baseDir = baseDir + "\\";
+			}
+			userPropsDir = baseDir + this.getDefaultProperty( "eleminator.userPropertiesPath.windows" );
+		} else { // Unknown OS
+			// Append a trailing path separator charactor, if necessary
+			if (baseDir.charAt( baseDir.length() - 1) != '/') {
+				baseDir = baseDir + "/";
+			}
+			userPropsDir = baseDir + this.getDefaultProperty( "eleminator.userPropertiesPath.unknownOS" );
+		}
+		// Get the full path to the file
+		userPropsPath = userPropsDir + this.getDefaultProperty( "eleminator.userPropertiesFileName" );
+		
+		// If no user properties file exists, create a new properties file with a
+		// list of default keys
+		if (new File(userPropsPath).exists() == false) {
+			System.err.println( "DIAG: Properties file not found. Creating..." );
+			// Get the default keys
+			PropertyResourceBundle defaultKeys =
+					(PropertyResourceBundle) ResourceBundle.getBundle( userListBundleName );
+			Properties defaultUserProps = new Properties();
+			keyList = defaultKeys.getKeys();
+			while (keyList.hasMoreElements()) {
+				defaultUserProps.setProperty( (String)keyList.nextElement(), "" );
+			}
+			// Write the default keys to a new user properties file
+			try {
+				new File( userPropsDir ).mkdirs();
+				new File( userPropsPath ).createNewFile();
+				FileOutputStream out = new FileOutputStream( userPropsPath );
+				defaultUserProps.store( out, "user properties file" );
+			} catch (FileNotFoundException fnfe) {
+				System.err.println( "Error: Unable to find user properties file" );
+				fnfe.printStackTrace();
+			} catch (IOException ioe) {
+				System.err.println( "Error: Unable to save user properties file" );
+				ioe.printStackTrace();
+			}
 		}
 		
 		// Load the user properties file
-		String userPropsFile = this.getDefaultProperty( "eleminator.userpropsfile" );
 		userProps = new Properties( defaultProps );
+		FileInputStream in = null;
+		
 		try {
-			in = new FileInputStream( this.getProperty( "eleminator.userpropsfile" ) );
+			in = new FileInputStream( userPropsPath );
 			userProps.load( in );
 		} catch (FileNotFoundException fnfe) {
-			System.err.println( "Error: Could not find default user file" );
+			System.err.println( "Error: Unable to find user properties file" );
 			fnfe.printStackTrace();
 		} catch (IOException ioe ) {
 			System.err.println( "Error: Unable to load user properties file" );
 			ioe.printStackTrace();
 		}
+	}
+	
+	/**
+	 * Returns an instance of the Parameters class. This ensures that at most
+	 * one instance of Parameters exists at a time, in accordance with the
+	 * Singleton pattern.
+	 */
+	public static Parameters getInstance() {
+		return currentParams;
 	}
 	
 	/**
@@ -119,6 +209,8 @@ public class Parameters {
 	 * default properties are found.
 	 *
 	 * @param key the name of the default property to retrieve
+	 *
+	 * @todo remove debugging code
 	 */
 	public String getDefaultProperty( String key ) {
 		return defaultProps.getProperty( key );
