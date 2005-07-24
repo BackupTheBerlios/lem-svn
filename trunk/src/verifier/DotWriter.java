1834b6d8fba1f62aef8a9ecdf99d0647f6444aeb
u3956099 <u3956099@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-07-24 10:24:03 +0000

    Header comment cleanup

--- trunk/src/verifier/DotWriter.java
+++ trunk/src/verifier/DotWriter.java
@@ -1,11 +1,22 @@
 /*
  * DotWriter.java
  *
- * Created on 23 July 2005, 21:53
+ * Copyright (C) 2005 Steven Michael Ring
  *
- * To change this template, choose Tools | Options and locate the template under
- * the Source Creation and Management node. Right-click the template and choose
- * Open. You can then make changes to the template in the Source Editor.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  */
 
 package verifier;
--- trunk/src/verifier/GenerateClassDiagramDialog.java
+++ trunk/src/verifier/GenerateClassDiagramDialog.java
@@ -1,7 +1,22 @@
 /*
  * GenerateClassDiagramDialog.java
  *
- * Created on 16 July 2005, 19:00
+ * Copyright (C) 2005 Steven Michael Ring
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  */
 
 package verifier;
@@ -20,9 +35,9 @@ import metamodel.Model;
 import metamodel.Subsystem;
 
 /**
+ * Dialog for generating class diagrams from LEM models.
  *
  * @author Toshio Nakamura
- * @todo use inheritance?
  */
 public class GenerateClassDiagramDialog extends javax.swing.JDialog {
 	
@@ -187,9 +202,6 @@ public class GenerateClassDiagramDialog extends javax.swing.JDialog {
 		selectedSubsystem = selectedDomain.getSubsystem( (String)cb.getSelectedItem() );
 	}//GEN-LAST:event_subsystemListActionPerformed
 		
-	/**
-	 * Add dot drawing code
-	 */
 	private void generateButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_generateButtonActionPerformed
 		if ("generate".equals( evt.getActionCommand() )) {
 			// Fail if no filename specified
