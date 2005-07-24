094c36565d320347fecfcd230c96838b5fe1fcc5
u3956099 <u3956099@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-07-24 10:23:29 +0000

    Responsible for generating dot code for class diagrams

---
+++ trunk/src/verifier/ClassWriter.java
@@ -0,0 +1,47 @@
+/*
+ * ClassWriter.java
+ *
+ * Copyright (C) 2005 Steven Michael Ring
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ */
+
+package verifier;
+
+import java.util.Collection;
+import metamodel.Domain;
+
+/**
+ * Generates dot code for class diagrams.
+ *
+ * @author Toshio Nakamura
+ */
+public class ClassWriter {
+	
+	/**
+	 * Generates dot code for class diagrams. Note that this procedure only
+	 * supports diagrams involving classes from a single domain.
+	 *
+	 * @param domain the domain inside which the class of interest reside
+	 * @param classList a list of class names (in String format)
+	 * that should  be included in the diagram
+	 * @todo fill in dot generation code
+	 * @todo print the class list
+	 */
+	public static String dumpDot( Domain domain, Collection classList ) {
+		return "This is currently a stub procedure only.";
+	}
+}
