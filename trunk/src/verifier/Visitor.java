71855eee785661a1bf6e14ba767ae06bf87983d3
u2567446 <u2567446@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-07-27 12:09:21 +0000

    Changed the visit with LemStatementList to LemActionList

--- trunk/src/verifier/Visitor.java
+++ trunk/src/verifier/Visitor.java
@@ -442,7 +442,7 @@ public class Visitor implements LemParserVisitor {
 		node.childrenAccept( this, data );
 		return data;
 	}
-	  public Object visit(LEMStatementList node, Object data) throws metamodel.LemException { 
+	  public Object visit(LEMActionList node, Object data) throws metamodel.LemException { 
 		logVisit( node, data );
 		node.childrenAccept( this, data );
 		return data;
