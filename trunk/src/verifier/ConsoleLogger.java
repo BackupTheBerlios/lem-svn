e9bfdaeede2188870bccfc1ac48db9cf5d64dba5
u3950565 <u3950565@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-07-28 01:51:56 +0000

    Added Javadoc to Object Creation

--- trunk/src/verifier/ConsoleLogger.java
+++ trunk/src/verifier/ConsoleLogger.java
@@ -1,43 +1,69 @@
+/*
+ * ConsoleLogger.java
+ *
+ */
+
 package verifier;
 
 import java.util.Iterator;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
+/**
+ * Receives all activity by the runtime and logs desired activity to the console 
+ * and a text file.
+ * Logging occurs at one of two levels, Level 1 which only logs events and Level 2
+ * which logs all activity. 
+ * Log messages include:
+ *   - ObjectCreation
+ *   - ObjectDeletion
+ *
+ * @author  Donna Aloe
+ */
 public class ConsoleLogger implements runtime.LemEventListener {
     
     static Logger logger = Logger.getRootLogger();
     private int counter = 0;
 
+
+    /**
+     * 
+     * Creates the text file in which all log messages are to written to 
+     *  
+     * @param c The context in which the console logger is operating
+     */
     public ConsoleLogger(runtime.Context c) {
 	c.addLemEventListener(this);
 	PropertyConfigurator.configure("eventLog.txt");
 	
     }
 
+    /**
+     * Set the level of loging to Level 1
+     */
      public void levelDebug() {
 	 //logger.setLevel(Level.DEBUG);
     }
 
+    /**
+     * Set the level of loging to Level 2
+     */
     public void levelInfo(){
 	//logger.setLevel(Level.INFO);
     }
     
-//    public void objectCreated(ObjectObject ob) {
-//
-//	// this might not be the way this should be done it may have to
-//	// be: ob.state.name ?
-//	String message = " OC: (" + ob.name() + ") [" + ob.state() + "]";
-//
-//	logger.debug(counter + message);
-//	counter ++;
-//    }
+
+    /**
+     * Called by the runtime when a CreateAction has been implemented
+     * and a new object has been created.
+     * A log message is then written to the console and the text file
+     *
+     * @param event The Object Creation event to be logged
+     **/
 
     public void objectCreated(runtime.LemObjectCreationEvent event) {
-        // this might not be the way this should be done it may have to
-	// be: ob.state.name ?
         runtime.Object o = event.getCreatedObject();
-	String message = " New Object created, instance(s): ";
+	String message = " OC ";
         for( Iterator i = o.getInstances().iterator(); i.hasNext(); ) {
             runtime.Instance in = (runtime.Instance)i.next();
             
