e5066f3ae516cb4570f59ebe7e37284242952b5f
u4128551 <u4128551@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-08-04 03:17:26 +0000

    First revision of Scenario node, JTree element.

---
+++ trunk/src/verifier/ScenarioNode.java
@@ -0,0 +1,68 @@
+/*
+ * ScenarioNode.java
+ *
+ * Created on 4 August 2005, 00:42
+ */
+package verifier;
+
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+import metamodel.Scenario;
+import runtime.DomainContext;
+import runtime.Interpreter;
+import runtime.LemRuntimeException;
+/**
+ *
+ * @author David Gavin
+ */
+public class ScenarioNode extends AbstractDescriptionNode  {
+   Scenario scenario; 
+    /** Creates a new instance of ScenarioNode */
+    public ScenarioNode(Scenario scenario) {
+        this.scenario = scenario;
+    }
+        /**
+     * Returns name property of LEM State object.
+     * @return the State name.
+     */
+    public String toString()  {
+        return "Scenario : " + scenario.getName() ;
+    }
+    /**
+     * Returns the description property of the LEM State object.
+     * @return the State description.
+     */
+    public String getDescription()  {
+        if (scenario.getDescription() != null)
+            return trim(scenario.getDescription()) ;
+        else
+            return "" ;
+    }
+    /**
+    * Returns the ContextMenu based on the LEM State object.
+    * @return the State ContextMenu.
+    */
+    public JPopupMenu getContextMenu() {
+        JPopupMenu contextMenu = new JPopupMenu();
+        JMenuItem executeProcedure = new JMenuItem();
+        executeProcedure.setText("Execute");
+        executeProcedure.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                executeScenario();
+            }
+        });
+        contextMenu.add(executeProcedure);
+        return contextMenu;
+    }
+    public void executeScenario() {
+        runtime.DomainContext d = new DomainContext();
+        ConsoleLogger c = new ConsoleLogger(d);
+        runtime.Interpreter i = new Interpreter(null);
+        try{
+            i.executeBlock(scenario.getActionBlock(),  d);
+        } catch(LemRuntimeException e) {
+            System.out.println(e);
+        }
+    }
+    
+}
