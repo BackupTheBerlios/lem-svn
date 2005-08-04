90c8e01edf59588d5370b85383626cb699902eb3
u4128551 <u4128551@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-08-04 04:06:57 +0000

    Creation of Transitions and States subtrees are now conditional.

--- trunk/src/verifier/StateMachineNode.java
+++ trunk/src/verifier/StateMachineNode.java
@@ -14,8 +14,8 @@ import metamodel.StateMachine;
 import metamodel.Transition;
 
 /**
- * Tree node appearing inside a ClassNode. For graphically representing and 
- * "holding" a LEM StateMachine object. Has StateNodes and TransitionNodes as 
+ * Tree node appearing inside a ClassNode. For graphically representing and
+ * "holding" a LEM StateMachine object. Has StateNodes and TransitionNodes as
  * children.
  * @author sjr
  */
@@ -23,51 +23,53 @@ public class StateMachineNode extends AbstractDescriptionNode {
     /**The LEM StateMachine object that StateMachineNode contains*/
     StateMachine machine = null;
     
-    /** 
+    /**
      * Creates a new instance of StateMachineNode. Creates a sublevel containing
-     * only StateNodes called stateLevel and a sublevel containing only 
-     * TransitionNodes called transitionLevel. Creates StateNodes based on LEM 
-     * State objects in LEM StateMachine object and places them in stateLevel. 
-     * Creates TransitionNodes based on LEM Transition objects in LEM 
-     * StateMachine object and places them in transitionLevel. LEM Transition 
-     * objects are checked for LEM InitialisingTransition objects. If a LEM 
-     * InitilisingTransition object is found an additional "Create ->" is placed 
+     * only StateNodes called stateLevel and a sublevel containing only
+     * TransitionNodes called transitionLevel. Creates StateNodes based on LEM
+     * State objects in LEM StateMachine object and places them in stateLevel.
+     * Creates TransitionNodes based on LEM Transition objects in LEM
+     * StateMachine object and places them in transitionLevel. LEM Transition
+     * objects are checked for LEM InitialisingTransition objects. If a LEM
+     * InitilisingTransition object is found an additional "Create ->" is placed
      * in the name of the corresponding TransitionNode object.
      * @param m LEM StateMachine object.
      */
     public StateMachineNode( StateMachine m ) {
         this.machine = m;
-                
+        
         if( m.getStateList() == null ) return;
         
-        DefaultMutableTreeNode stateLevel = new DefaultMutableTreeNode("States");
-        DefaultMutableTreeNode transitionLevel = new DefaultMutableTreeNode("Transitions");
-        
         Iterator i = machine.getStateList().listIterator();
-        while( i.hasNext() ) {          
-            stateLevel.add( new StateNode((metamodel.State) i.next()) ) ;                                
+        if(i.hasNext()) {
+            DefaultMutableTreeNode stateLevel = new DefaultMutableTreeNode("States");
+            while( i.hasNext() ) {
+                stateLevel.add( new StateNode((metamodel.State) i.next()) ) ;
+            }
+            add( stateLevel );
         }
         
         i = machine.getTransitionList().listIterator();
-        while( i.hasNext() ) {
-            Transition t = (Transition)i.next();
-            String name;
-            if( t instanceof InitialisingTransition ) {
-                name = "CREATE ->" + t.getToState().getName();
-            } else {
-                name = t.getFromState().getName() + " -> " + t.getToState().getName();
+        if(i.hasNext()) {
+            DefaultMutableTreeNode transitionLevel = new DefaultMutableTreeNode("Transitions");
+            while( i.hasNext() ) {
+                Transition t = (Transition)i.next();
+                String name;
+                if( t instanceof InitialisingTransition ) {
+                    name = "CREATE ->" + t.getToState().getName();
+                } else {
+                    name = t.getFromState().getName() + " -> " + t.getToState().getName();
+                }
+                
+                transitionLevel.add( new TransitionNode( t , name ));
             }
-            
-            transitionLevel.add( new TransitionNode( t , name ));
+            add( transitionLevel );
         }
-        
-        add( stateLevel );
-        add( transitionLevel );
     }
     /**
      * Returns string "State machine".
      * @return "State machine".
-     */ 
+     */
     public String toString() {
         
         return "State machine";
@@ -76,19 +78,17 @@ public class StateMachineNode extends AbstractDescriptionNode {
      * Returns the description property of the LEM StateMachine object.
      * @return the StateMachine description.
      */
-    public String getDescription()
-    {
+    public String getDescription() {
         if (machine.getDescription() != null)
             return trim(machine.getDescription());
-        else 
-            return "" ; 
+        else
+            return "" ;
     }
     /**
      * Returns the ContextMenu based on the LEM StateMachine object.
      * @return the StateMachine ContextMenu.
-     */ 
-    public JPopupMenu getContextMenu()
-    {
+     */
+    public JPopupMenu getContextMenu() {
         JPopupMenu ContextMenu = new JPopupMenu();
         return ContextMenu;
     }
