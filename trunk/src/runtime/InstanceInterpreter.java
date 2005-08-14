5d7c088e6550b9e672c62828c8d8f6935a10d59d
u3958283 <u3958283@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-08-14 14:45:22 +0000

    changed the initialization method to check for signals until it receives a signal corresponding a initTransition.

--- trunk/src/runtime/InstanceInterpreter.java
+++ trunk/src/runtime/InstanceInterpreter.java
@@ -10,18 +10,13 @@ import java.util.Collection;
 import java.util.HashMap;
 import runtime.Context ;
 
-
 /**
  * This is the main InstanceInterpreter file.
- * @author npiggin
- * @author sjr
- * @todo javadoc
+ * @author shuku 
  */
 public class InstanceInterpreter extends java.lang.Thread{
     /** the instance to which this Interpreter belongs to **/
     private runtime.Instance instance;
-    
-    private runtime.Object currentObject;
     /** static constants **/
     final static int STOPPED = 1 ;
     final static int RUNNING = 2 ;
@@ -29,39 +24,90 @@ public class InstanceInterpreter extends java.lang.Thread{
     private int state = STOPPED ;
     
     /** Creates a new instance of Interpreter
-     * @param obj is the object that the Interpreter is
-     * executing the procedures for.
+     * @param instance the instance to which this interpreter belongs.
      */
     public InstanceInterpreter(runtime.Instance instance) {
         this.state = STOPPED ;
         this.instance = instance ;
         init() ;
-        start() ;
+        start() ;     
     }
     
-    /** this method will start the interpretion thread by getting the next signal,
-     *traversing the corresponding transition and interpreting the procedure of the
-     *corresponding state of the statemachine.
+    /** this method will start the interpretion thread by keep getting the next signal
+     *until there is a signal which would lead it to the next state, it will run the procedure associated
+     *with the new state ( only if the new state is not a DeletionState ).
      */
     public void init() {
         this.state = RUNNING ;
         metamodel.Class c = instance.getInstanceClass() ;
         metamodel.StateMachine m = c.getStateMachine() ;
         try {
+            signal :
+            while ( true ) {
+                Signal s = instance.getNextSignal() ;
+                int tCount = m.getInitialisingTransitionCount() ;
+                if ( tCount > 0 ) {
+                    Transition[] ts =  m.getInitialisingTransitions() ;
+                    for ( int i  = 0 ; i < tCount ; i++) {
+                        Transition t = ts[i] ;
+                        if (s.getEvent().getName().equals( t.getEvent().getName() )) {
+                            metamodel.State newState = t.getToState() ;
+                            instance.currentState = newState ;
+                            if ( newState instanceof NonDeletionState) {
+                                Procedure p = newState.getProcedure() ;
+                                Context newContext = new Context() ;
+                                Interpreter interpreter = new Interpreter(null) ;
+                                interpreter.interpret( p , newContext ) ;
+                            }
+                            break signal ;
+                        }
+                    }
+                }
+                sleep(100) ;
+            }
+        } catch (Exception e) {
+            this.state = STOPPED ;
+        }
+        this.state = STOPPED ;
+    }
+    
+    
+    /** will run the thread until it enters a deletionstate **/
+    public void run() {
+        while ( instance.currentState instanceof NonDeletionState && isAlive() ) {
+            yield() ;
+            advance() ;
+            try {
+                sleep( 100 ) ;
+            }catch (Exception e ) {}
+        }
+    }
+    
+    /** Advances the statemachine by one state (if possible)
+     *and will run the procedure associated with the new state if the new state is not a
+     *DeletionState.
+     **/
+    private void advance() {
+        this.state = RUNNING ;
+        metamodel.Class c = instance.getInstanceClass() ;
+        metamodel.StateMachine m = c.getStateMachine() ;
+        metamodel.State currentState = instance.currentState ;
+        try {
             Signal s = instance.getNextSignal() ;
-            int tCount = m.getInitialisingTransitionCount() ;
+            int tCount = m.getStateTransitionCount() ;
             if ( tCount > 0 ) {
-                Transition[] ts =  m.getInitialisingTransitions() ;
+                Transition[] ts =  m.getTransitions() ;
                 for ( int i  = 0 ; i < tCount ; i++) {
                     Transition t = ts[i] ;
-                    if (s.getEvent().getName().equals( t.getEvent().getName() )) {
+                    if (s.getEvent().getName().equals( t.getEvent().getName() )
+                    && t.getFromState().getName().equals(currentState.getName()) ) {
                         metamodel.State newState = t.getToState() ;
                         instance.currentState = newState ;
                         if ( newState instanceof NonDeletionState) {
-                        Procedure p = newState.getProcedure() ;
-                        Context newContext = new Context() ;
-                        Interpreter interpreter = new Interpreter(null) ;
-                        interpreter.interpret( p , newContext ) ;
+                            Procedure p = newState.getProcedure() ;
+                            Context newContext = new Context() ;
+                            Interpreter interpreter = new Interpreter(null) ;
+                            interpreter.interpret( p , newContext ) ;
                         }
                     }
                 }
@@ -72,59 +118,18 @@ public class InstanceInterpreter extends java.lang.Thread{
         this.state = STOPPED ;
     }
     
-    
-    /** will run the thread until it enters a deletionstate **/        
-    public void run() {
-        while ( instance.currentState instanceof NonDeletionState && isAlive() ) {
-            yield() ; 
-            advance() ; 
-            try {
-                sleep( 100 ) ; 
-            }catch (Exception e ) {}
-        }
-    }
-    
-    /** Advances the statemachine by one state (if possible) **/    
-    public void advance() {
-        this.state = RUNNING ;
-        metamodel.Class c = instance.getInstanceClass() ;
-        metamodel.StateMachine m = c.getStateMachine() ;
-        metamodel.State currentState = instance.currentState ;
-        try {
-            Signal s = instance.getNextSignal() ;
-            int tCount = m.getStateTransitionCount() ;
-            if ( tCount > 0 ) {
-                Transition[] ts =  m.getTransitions() ; 
-                for ( int i  = 0 ; i < tCount ; i++) {
-                    Transition t = ts[i] ;
-                    if (s.getEvent().getName().equals( t.getEvent().getName() )
-                    && t.getFromState().getName().equals(currentState.getName()) ) {
-                        metamodel.State newState = t.getToState() ;
-                        instance.currentState = newState ;
-                        if ( newState instanceof NonDeletionState) {
-                        Procedure p = newState.getProcedure() ;
-                        Context newContext = new Context() ;
-                        Interpreter interpreter = new Interpreter(null) ;
-                        interpreter.interpret( p , newContext ) ;
-                        } 
-                    }
-                }
-            }
-        } catch (Exception e) {
-            this.state = STOPPED ;
-        }
-        this.state = STOPPED ;
-    }
-    
+    /** This method will stop the interpretion of the current instance
+     *The interpretor will stop when the a state has been fully executed
+     */
     public void stopInterpreter() throws Exception {
-        if (state == STOPPED ) {
-            this.stop() ;
-        } else {
-            sleep ( 100 ) ;
-            stopInterpreter() ;
+        while ( state == RUNNING) {
+            sleep( 100 ) ;
         }
+        this.stop() ;
     }
     
+    /**This method will resume execution of the current state
+    */
     public void resumeInterpreter() throws Exception {
         resume() ;
     }
