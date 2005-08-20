2b2261c73d3a47f11901ce6f2b48c9e8d9658765
u2567446 <u2567446@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-08-20 01:53:40 +0000

    Modified the variable reference to a Hashmap and added various function to check if a variable exist in the block or it's ancestor blocks.

--- trunk/src/metamodel/ActionBlock.java
+++ trunk/src/metamodel/ActionBlock.java
@@ -3,6 +3,7 @@ package metamodel;
 import java.util.LinkedList;
 import java.util.Collection;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Represents a LEM action block (a block containing a local variable scope,
@@ -11,25 +12,49 @@ import java.util.ArrayList;
  * @author npiggin
  */
 public class ActionBlock {
-    private Collection variableDeclarations;
+    //private Collection variableDeclarations;
+    private HashMap variableDeclarations;
     private LinkedList actionList;
+    private ActionBlock parent=null;
     
     /** Creates a new instance of ActionBlock */
     public ActionBlock() {
 	actionList = new LinkedList();
-	variableDeclarations = new ArrayList();
+	//variableDeclarations = new ArrayList();
+        variableDeclarations = new HashMap();
     }
     
     /** returns the local variables declared in this block */
     public Collection getVariableDeclarations() {
-	return variableDeclarations;
+	//return variableDeclarations;
+        return variableDeclarations.values();
     }
 
-    /** adds a variable declaration to the block */
-    public void addVariableDeclaration(VariableDeclaration v) {
-	variableDeclarations.add(v);
+    /** adds a variable declaration to the block 
+     *  return false of the variable already exist in the block
+     *  and true to indicate variable has been added.
+     */
+    public boolean addVariableDeclaration(VariableDeclaration v) {
+	//variableDeclarations.add(v);
+        if(containsVariable(v.getVariableName()))
+            return false;
+        variableDeclarations.put(v.getVariableName(), v);
+        return true;
     }
 
+    public boolean isValidVariable(String variable) {
+        if(containsVariable(variable))
+            return true;
+        else if(parent != null) {
+            return parent.isValidVariable(variable);
+        }
+        return false;
+    }
+    
+    public boolean containsVariable(String variable) {
+        return variableDeclarations.containsKey(variable);
+    }
+    
     /** returns the list of actions performed by this block */
     public LinkedList getActions() {
 	return actionList;
@@ -37,6 +62,16 @@ public class ActionBlock {
 
     /** adds the next action to be performed by the block */
     public void addAction(Action a) {
-	actionList.add(a);
+        actionList.add(a);
     }
+    
+    /** sets the action block in which this block resides in */
+    public void setParent(ActionBlock p) {
+        this.parent = p;
+    }
+    
+    /** returns the action block in which this block resides in */
+    public ActionBlock getParent() {
+        return this.parent;
+    }    
 }
