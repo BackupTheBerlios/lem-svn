f612fa6dfa51c419a0f5fec8bbc56a741c3cb457
u3958283 <u3958283@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-09-03 08:04:55 +0000

    Added a reference of the main window inside ModelTreeNode.

--- trunk/src/verifier/ModelTreeNode.java
+++ trunk/src/verifier/ModelTreeNode.java
@@ -12,47 +12,54 @@ import metamodel.Domain;
 import metamodel.Model;
 
 /**
- * Tree node appearing inside a ModelTreePanel. For graphically representing and 
+ * Tree node appearing inside a ModelTreePanel. For graphically representing and
  * "holding" LEM Model objects. Has DomainNode as a child.
  * @author sjr
  */
 public class ModelTreeNode extends AbstractDescriptionNode {
     /**LEM Model object that ModelTreeNode contains*/
     Model model;
-    
-    /** 
-    * Creates a new instance of ModelTreeNode. Creates DomainNode branches 
-    * based on LEM Model object.
-    * @param m the LEM Model object.
-    */
-    public ModelTreeNode( Model m ) {
+    private ModelTreePanel view ;
+    /**
+     * Creates a new instance of ModelTreeNode. Creates DomainNode branches
+     * based on LEM Model object.
+     * @param m the LEM Model object.
+     */
+    public ModelTreeNode( Model m , ModelTreePanel view ) {
         this.model = m;
+        this.view = view ; 
         Iterator i = m.getDomains().values().iterator();
         
         while( i.hasNext() ) {
-            add( new DomainNode( (Domain)i.next() ));
+            DomainNode d = new DomainNode((Domain)i.next() , view) ;
+            //d.setView(view) ;
+            add( d ) ;
+            //add( new DomainNode( (Domain)i.next() ));
         }
     }
+    
+    public void setView(ModelTreePanel view) {
+        this.view = view ;
+    }
     /**
-    * Returns name property of LEM Model object.
-    * @return the Model name.
-    */ 
+     * Returns name property of LEM Model object.
+     * @return the Model name.
+     */
     public String toString() {
         return "Model " + model.getName();
     }
     /**
-    * Returns the description property of the LEM Model object.
-    * @return the Model description.
-    */    
+     * Returns the description property of the LEM Model object.
+     * @return the Model description.
+     */
     public String getDescription(){
         return trim(model.getDescription());
     }
     /**
-    * Returns the ContextMenu based on the LEM Model object.
-    * @return the Model ContextMenu.
-    */
-    public JPopupMenu getContextMenu()
-    {
+     * Returns the ContextMenu based on the LEM Model object.
+     * @return the Model ContextMenu.
+     */
+    public JPopupMenu getContextMenu() {
         JPopupMenu ContextMenu = new JPopupMenu();
         return ContextMenu;
     }
