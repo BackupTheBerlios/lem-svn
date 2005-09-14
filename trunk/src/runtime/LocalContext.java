f8a9c639bdbf2bc92cf436cccbb077d98dfad299
u3957053 <u3957053@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-09-14 05:49:32 +0000

    fix indentation

--- trunk/src/runtime/LocalContext.java
+++ trunk/src/runtime/LocalContext.java
@@ -15,50 +15,50 @@ import java.util.HashMap;
  * @author u4128551
  */
 public class LocalContext extends Context {
-    /**
-     * A collection of all variables local to the context
-     */
-    private List variableList = new LinkedList();
+	/**
+	 * A collection of all variables local to the context
+	 */
+	private List variableList = new LinkedList();
 
-    /**
-     * A map of all variables local to the context
-     */
-    private HashMap variableMap = new HashMap();
+	/**
+	 * A map of all variables local to the context
+	 */
+	private HashMap variableMap = new HashMap();
    
-    /** Creates a new instance of LocalContext */
-    public LocalContext(Context inContext) {
-        super(inContext);
-    }
+	/** Creates a new instance of LocalContext */
+	public LocalContext(Context inContext) {
+		super(inContext);
+	}
  
-    /**
-     * Gets the named variable from this context or any parent contexts.
-     *
-     * @param name the name of the variable
-     * @return the variable, or null if there is no variable with that name
-     */
-    public Variable getVariable( String name ) {
-        Variable v = (Variable)variableMap.get( name );
-        
-        if( v == null && parentContext != null ) {
-            // Search parent contexts
-            v = parentContext.getVariable( name );
-        }
-        
-        return v;
-    }
-    
-    /**
-     * Adds the given variable to this context with the given identifying name.
-     * 
-     * @param name the identifier of the variable
-     * @param variable the variable to be added
-     */
-    public void addVariable( String name, Variable variable ) {
-        variableMap.put( name, variable );
-	variableList.add( variable );
-    }
-    
-    public List getVariableList() {
-	return variableList;
-    }
+	/**
+	 * Gets the named variable from this context or any parent contexts.
+	 *
+	 * @param name the name of the variable
+	 * @return the variable, or null if there is no variable with that name
+	 */
+	public Variable getVariable( String name ) {
+		Variable v = (Variable)variableMap.get( name );
+		
+		if( v == null && parentContext != null ) {
+			// Search parent contexts
+			v = parentContext.getVariable( name );
+		}
+		
+		return v;
+	}
+	
+	/**
+	 * Adds the given variable to this context with the given identifying name.
+	 * 
+	 * @param name the identifier of the variable
+	 * @param variable the variable to be added
+	 */
+	public void addVariable( String name, Variable variable ) {
+		variableMap.put( name, variable );
+		variableList.add( variable );
+	}
+	
+	public List getVariableList() {
+		return variableList;
+	}
 }
