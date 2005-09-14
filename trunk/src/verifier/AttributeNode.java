997d9b64cc5933feef41482953410676c39f26b4
u3958283 <u3958283@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-09-14 05:21:47 +0000

    Modified attribute node to display dynamic description.

--- trunk/src/verifier/AttributeNode.java
+++ trunk/src/verifier/AttributeNode.java
@@ -5,51 +5,70 @@
  */
 
 package verifier;
-
-import javax.swing.JFrame;
+import java.awt.Color;
 import javax.swing.JPopupMenu;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
 import metamodel.Attribute;
 
 /**
- * Tree node appearing appearing inside a ClassNode. For graphically 
+ * Tree node appearing appearing inside a ClassNode. For graphically
  * representing and "holding" LEM attribute objects. Has no children.
  * @author sjr
  */
 public class AttributeNode extends AbstractDescriptionNode {
-    /**The LEM attribute that AttributeNode contains.*/
-    private Attribute attribute = null;
-    private LoggerFrame frame = null ; 
-    /** Creates a new instance of AttributeNode
-     * @param a the LEM attribute object.
-     */
-    public AttributeNode( Attribute a , LoggerFrame frame ) {
-        this.attribute = a;
-		this.frame = frame ; 
-    }
-    /** Returns the name of the attribute followed by the type of the attribute 
-     * @return the name of the attribute.
-     */
-    public String toString() {
-        return attribute.getName() + " : " + attribute.getType().getName();
-    }
-    /** 
-     * Returns the description of the attribute, will return an empty string if
-     * description is null.
-     * @return the Description
-     */
-    public String getDescription() {        
-        if (attribute.getDescription() != null)
-            return trim(attribute.getDescription() );        
-        else
-            return "" ;                         
-    }
-    /**
-     * Creates and returns a JPopupMenu based on the specified attribute.
-     * @return the ContextMenu.
-     */
-    public JPopupMenu getContextMenu()
-    {
-        JPopupMenu ContextMenu = new JPopupMenu();
-        return ContextMenu;
-    }
+	/**The LEM attribute that AttributeNode contains.*/
+	private Attribute attribute = null;
+	private LoggerFrame frame = null ;
+	/** Creates a new instance of AttributeNode
+	 * @param a the LEM attribute object.
+	 */
+	public AttributeNode( Attribute a , LoggerFrame frame ) {
+		this.attribute = a;
+		this.frame = frame ;
+	}
+	/** Returns the name of the attribute followed by the type of the attribute
+	 * @return the name of the attribute.
+	 */
+	public String toString() {
+		return attribute.getName() + " : " + attribute.getType().getName();
+	}
+	/**
+	 * Returns the description of the attribute, will return an empty string if
+	 * description is null.
+	 * @return the Description
+	 */
+	public String getDescription() {
+		if (attribute.getDescription() != null)
+			return trim(attribute.getDescription() );
+		else
+			return "" ;
+	}
+	/**
+	 * Creates and returns a JPopupMenu based on the specified attribute.
+	 * @return the ContextMenu.
+	 */
+	public JPopupMenu getContextMenu() {
+		JPopupMenu ContextMenu = new JPopupMenu();
+		ContextMenu.add("Descriptions") ;
+		return ContextMenu;
+	}
+	
+	public StyledDocument getDynamicDescription() {
+		StyledDocument doc = new StyledDocument() ;
+		String name = attribute.getName() ;
+		String typeName = attribute.getType().getName() ;
+
+		SimpleAttributeSet s = new SimpleAttributeSet();
+		StyleConstants.setFontFamily(s, "Times New Roman");
+		StyleConstants.setFontSize(s, 14);
+		StyleConstants.setBold(s, true);
+		StyleConstants.setForeground(s, Color.blue);
+		StyledElement element = new StyledElement( name + ":"+ typeName , s) ;
+		doc.addStyle(element) ; 
+		
+			
+		return doc;
+	}
+	
 }
