46904ce12289505c0dc50376adabbee2d2b9dd2e
u4128551 <u4128551@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-09-15 04:12:21 +0000

    Added GPL boilerplate. Removed incorrect casting of a variable object to a parameter object. Changed reference to ContextParameterNode to ContextVariableNode.

--- trunk/src/verifier/ContextSignalNode.java
+++ trunk/src/verifier/ContextSignalNode.java
@@ -1,11 +1,22 @@
 /*
  * SignalNode.java
  *
- * Created on September 14, 2005, 1:35 PM
+ * Copyright (C) 2005 David Gavin
  *
- * To change this template, choose Tools | Options and locate the template under
- * the Source Creation and Management node. Right-click the template and choose
- * Open. You can then make changes to the template in the Source Editor.
+ * This program is free software; you can redistribute it and/or  
+ * modify it under the terms of the GNU General Public License  
+ * as published by the Free Software Foundation; either version 2  
+ * of the License, or (at your option) any later version.  
+ *  
+ * This program is distributed in the hope that it will be useful,  
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of  
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  
+ * GNU General Public License for more details.  
+ * 
+ * You should have received a copy of the GNU General Public License  
+ * along with this program; if not, write to the Free Software  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,  
+ * USA. 
  */
 
 package verifier;
@@ -13,12 +24,12 @@ package verifier;
 import java.util.Iterator;
 import javax.swing.JPopupMenu;
 import javax.swing.tree.DefaultMutableTreeNode;
-import metamodel.Parameter;
 import runtime.Signal;
+import runtime.Variable;
 
 /**
  *
- * @author u4128551
+ * @author David Gavin
  */
 public class ContextSignalNode extends AbstractDescriptionNode {
 	Signal thisSignal ; 
@@ -34,7 +45,7 @@ public class ContextSignalNode extends AbstractDescriptionNode {
 		if(parameters.hasNext()) {
 			add(parameterLevel);
 			while(parameters.hasNext()) {
-				parameterLevel.add(new ContextParameterNode((Parameter)parameters.next(),frame));
+				parameterLevel.add(new ContextVariableNode((Variable)parameters.next(),frame));
 			}
 		}
 		}
