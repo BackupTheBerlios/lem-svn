80aa5a9516938b51bb95de6d8c99f372c2ca930e
u3957053 <u3957053@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-09-15 23:57:13 +0000

    Fix up indentation, add GPL boilerplate

--- trunk/src/runtime/DomainContext.java
+++ trunk/src/runtime/DomainContext.java
@@ -1,11 +1,22 @@
 /*
  * DomainContext.java
  *
- * Created on June 17, 2005, 12:52 PM
+ * Copyright (C) 2005 James Ring
  *
- * To change this template, choose Tools | Options and locate the template under
- * the Source Creation and Management node. Right-click the template and choose
- * Open. You can then make changes to the template in the Source Editor.
+ * This program is free software; you can redistribute it and/or  
+ * modify it under the terms of the GNU General Public License  
+ * as published by the Free Software Foundation; either version 2  
+ * of the License, or (at your option) any later version.  
+ *  
+ * This program is distributed in the hope that it will be useful,  
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of  
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  
+ * GNU General Public License for more details.  
+ * 
+ * You should have received a copy of the GNU General Public License  
+ * along with this program; if not, write to the Free Software  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,  
+ * USA. 
  */
 
 package runtime;
@@ -21,52 +32,53 @@ import java.util.ArrayList;
  * @author sjr
  */
 public class DomainContext extends Context {
-    public Debug debugObject;
+	public Debug debugObject;
 
-    /** 
-     * A collection of all runtime objects known to this context
-     */
-    private Collection objectList = new ArrayList();
+	/** 
+	 * A collection of all runtime objects known to this context
+	 */
+	private Collection objectList = new ArrayList();
 
-    /** Creates a new instance of DomainContext */
-    public DomainContext() {
-	    super(null);
-	    debugObject = new Debug(this);
-    }
-
-    /**
-     * Adds the given object to this context
-     * @param inObject the object to add to the context
-     */
-    public synchronized void addObject(runtime.Object inObject) {
-        objectList.add(inObject);
-    }
-
-    /**
-     * Adds the given object to this context
-     * @param inObject the object to add to the context
-     */
-    public synchronized void delObject(runtime.Object o) throws LemRuntimeException {
-        if (!objectList.remove(o)) {
-		if (parentContext == null)
-			throw new LemRuntimeException("Tried to delete non-existant object");
-		parentContext.delObject(o);
+	/** Creates a new instance of DomainContext */
+	public DomainContext() {
+		super(null);
+		debugObject = new Debug(this);
 	}
-    }
 
-    /**
-     * Adds the given collection of objects to this context
-     * @param inObjects the objects to add to the context
-     */
-    public synchronized void addObjects(Collection inObjects) throws LemRuntimeException {
-	    objectList.addAll(inObjects);
-    }
-    
-    /**
-     * Returns the list of objects in this context
-     * @returns the list of objects.
-     */
-    public Collection getObjectList() {
-	    return objectList;
-    }
+	/**
+	 * Adds the given object to this context
+	 * @param inObject the object to add to the context
+	 */
+	public synchronized void addObject(runtime.Object inObject) {
+		objectList.add(inObject);
+	}
+
+	/**
+	 * Adds the given object to this context
+	 * @param inObject the object to add to the context
+	 */
+	public synchronized void delObject(runtime.Object o) throws LemRuntimeException {
+		if (!objectList.remove(o)) {
+			if (parentContext == null)
+				throw new LemRuntimeException("Tried to delete non-existant object");
+
+			parentContext.delObject(o);
+		}
+	}
+
+	/**
+	 * Adds the given collection of objects to this context
+	 * @param inObjects the objects to add to the context
+	 */
+	public synchronized void addObjects(Collection inObjects) throws LemRuntimeException {
+		objectList.addAll(inObjects);
+	}
+	
+	/**
+	 * Returns the list of objects in this context
+	 * @returns the list of objects.
+	 */
+	public Collection getObjectList() {
+		return objectList;
+	}
 }
