87fa83d48a88bfa1c0b1137cc56c0fc4e2780afe
u4128551 <u4128551@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-09-15 04:16:23 +0000

    Added GPL boiler plate and removed reference to thisEvent.getSignature(). Was returning useless garbage.

--- trunk/src/verifier/ContextEventNode.java
+++ trunk/src/verifier/ContextEventNode.java
@@ -3,11 +3,24 @@
  *
  * Created on September 12, 2005, 4:24 PM
  *
- * To change this template, choose Tools | Options and locate the template under
- * the Source Creation and Management node. Right-click the template and choose
- * Open. You can then make changes to the template in the Source Editor.
+ * Copyright (C) 2005 Shokouhmand Torabi
+ * Copyright (C) 2005 David Gavin
+ *
+ * This program is free software; you can redistribute it and/or  
+ * modify it under the terms of the GNU General Public License  
+ * as published by the Free Software Foundation; either version 2  
+ * of the License, or (at your option) any later version.  
+ *  
+ * This program is distributed in the hope that it will be useful,  
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of  
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  
+ * GNU General Public License for more details.  
+ * 
+ * You should have received a copy of the GNU General Public License  
+ * along with this program; if not, write to the Free Software  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,  
+ * USA. 
  */
-
 package verifier;
 
 import javax.swing.JPopupMenu;
@@ -31,7 +44,7 @@ public class ContextEventNode extends AbstractDescriptionNode {
 	 * @return the Object name.
 	 */
 	public String toString() {
-		return thisEvent.getName() + " : " + thisEvent.getSignature() ;
+		return thisEvent.getName() + " : ";
 	}
 	/**
 	 * Returns the description property of the Object.
