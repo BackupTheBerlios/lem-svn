0f2cc3dabfecbc016359267d2d037b2e34bfa415
u4128551 <u4128551@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-09-15 04:09:23 +0000

    Added GPL boiler plate. Added ability to display the state machine and current state of the instance that the node reperesents.

--- trunk/src/verifier/InstanceNode.java
+++ trunk/src/verifier/InstanceNode.java
@@ -3,9 +3,23 @@
  *
  * Created on September 9, 2005, 2:51 PM
  *
- * To change this template, choose Tools | Options and locate the template under
- * the Source Creation and Management node. Right-click the template and choose
- * Open. You can then make changes to the template in the Source Editor.
+ * Copyright (C) 2005 Shokouhmand Torabi
+ * Copyright (C) 2005 David Gavin
+ *
+ * This program is free software; you can redistribute it and/or  
+ * modify it under the terms of the GNU General Public License  
+ * as published by the Free Software Foundation; either version 2  
+ * of the License, or (at your option) any later version.  
+ *  
+ * This program is distributed in the hope that it will be useful,  
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of  
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  
+ * GNU General Public License for more details.  
+ * 
+ * You should have received a copy of the GNU General Public License  
+ * along with this program; if not, write to the Free Software  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,  
+ * USA. 
  */
 
 package verifier;
@@ -16,6 +30,8 @@ import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
 import javax.swing.tree.DefaultMutableTreeNode;
 import metamodel.Attribute;
+import metamodel.State;
+import metamodel.StateMachine;
 import runtime.Instance;
 
 /**
@@ -40,6 +56,11 @@ public class InstanceNode extends AbstractDescriptionNode {
 			while ( i.hasNext() ) {
 				attributesLevel.add( new AttributeNode( (Attribute)i.next() , thisInstance, frame )) ;
 			}
+		}
+                StateMachine m = thisInstance.getInstanceClass().getStateMachine();
+		if( m != null ) {
+                    State currentState = thisInstance.getCurrentState();
+			add( new ContextStateMachineNode( m, currentState, frame ));
 		}
 	}
 	
