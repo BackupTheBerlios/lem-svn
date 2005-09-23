bd6892dd6016b37a1f6c0d310f7aebf0ae901484
u3957053 <u3957053@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-09-23 12:54:26 +0000

    add boilerplate, fix indentation

--- trunk/src/runtime/LemEvent.java
+++ trunk/src/runtime/LemEvent.java
@@ -1,11 +1,22 @@
 /*
  * LemEvent.java
  *
- * Created on June 17, 2005, 12:20 PM
+ * Copyright (C) 2005 James Ring
  *
- * To change this template, choose Tools | Options and locate the template under
- * the Source Creation and Management node. Right-click the template and choose
- * Open. You can then make changes to the template in the Source Editor.
+ * This program is free software; you can redistribute it and/or  
+ * modify it under the terms of the GNU General Public License  
+ * as published by the Free Software Foundation; either version 2  
+ * of the License, or (at your option) any later version.  
+ *  
+ * This program is distributed in the hope that it will be useful,  
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of  
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  
+ * GNU General Public License for more details.  
+ * 
+ * You should have received a copy of the GNU General Public License  
+ * along with this program; if not, write to the Free Software  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,  
+ * USA. 
  */
 
 package runtime;
@@ -25,27 +36,27 @@ import java.util.Iterator;
  * @author sjr
  */
 public abstract class LemEvent {
-    
-    /** Creates a new instance of LemEvent */
-    public LemEvent() {
-    }
+	
+	/** Creates a new instance of LemEvent */
+	public LemEvent() {
+	}
 
-    /**
-     * Notify the given event listener that this LemEvent has occurred.
-     *
-     * @param listener the LemEventListener to notify
-     */
-    public abstract void notify( LemEventListener listener );
+	/**
+	 * Notify the given event listener that this LemEvent has occurred.
+	 *
+	 * @param listener the LemEventListener to notify
+	 */
+	public abstract void notify( LemEventListener listener );
 
-    /**
-     * Notify all listeners of a given Context of this LemEvent's occurrence.
-     *
-     * @param context the Context to notify
-     */
-    public void notifyAll( Context context ) {
-        for( Iterator i = context.getLemEventListeners().iterator();
-                i.hasNext(); ) {
-            notify( (LemEventListener)i.next() );
-        }
-    }
+	/**
+	 * Notify all listeners of a given Context of this LemEvent's occurrence.
+	 *
+	 * @param context the Context to notify
+	 */
+	public void notifyAll( Context context ) {
+		for( Iterator i = context.getLemEventListeners().iterator();
+				i.hasNext(); ) {
+			notify( (LemEventListener)i.next() );
+		}
+	}
 }
