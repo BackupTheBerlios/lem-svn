337bf42d8c0a935ab4a5de353a886a6ea1de26e0
u3957053 <u3957053@6e2ef900-fc03-0410-88c8-97df3a172d98>
2005-10-02 06:42:34 +0000

    invokeMain on SplashWindow rather than spl to quell Jikes warning

--- trunk/src/splash/Splasher.java
+++ trunk/src/splash/Splasher.java
@@ -44,6 +44,6 @@ public class Splasher {
     public static void main(String[] args) {
           SplashWindow spl;
           spl = new SplashWindow("splash/eLEMinator.jpg", new java.awt.Frame(), 2000); //Insert splash image here
-          spl.invokeMain("verifier.Eleminator", args); //Insert target class here
+          SplashWindow.invokeMain("verifier.Eleminator", args); //Insert target class here
     }
 }
