e246e073f39b028a8c25f6813e0088828f38fcb2
sjr <sjr@6e2ef900-fc03-0410-88c8-97df3a172d98>
2006-02-10 06:49:39 +0000

    add GPL boilerplate, remove wildcard import statements

--- trunk/src/org/jdns/xtuml/tools/Lem.java
+++ trunk/src/org/jdns/xtuml/tools/Lem.java
@@ -1,22 +1,40 @@
+
 /*
  * Lem.java
  *
- * Created on October 29, 2004, 3:45 PM
+ * Copyright (C) 2006 Steven Ring
+ *
+ * This program is free software; you can redistribute it and/or  
+ * modify it under the terms of the GNU General Public License  
+ * as published by the Free Software Foundation; either version 2  
+ * of the License, or (at your option) any later version.  
+ *  
+ * This program is distributed in the hope that it will be useful,  
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of  
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  
+ * GNU General Public License for more details.  
+ * 
+ * You should have received a copy of the GNU General Public License  
+ * along with this program; if not, write to the Free Software  
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,  
+ * USA. 
  */
 
 package org.jdns.xtuml.tools;
 
-import com.sun.java_cup.internal.parser;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.io.Reader;
+import java.net.URL;
+
 import org.jdns.xtuml.metamodel.LemException;
 import org.jdns.xtuml.metamodel.Model;
-import org.jdns.xtuml.util.SourceBuffer;
-import java.io.*;
-import java.net.URL;
 import org.jdns.xtuml.parser.LEMModelDeclaration;
 import org.jdns.xtuml.parser.LemParser;
 import org.jdns.xtuml.parser.ParseException;
 import org.jdns.xtuml.parser.Token;
-
+import org.jdns.xtuml.util.SourceBuffer;
 
 /**
  * A command-line parser for LEM-coded models - LEM: a Language for Executable Models. 
