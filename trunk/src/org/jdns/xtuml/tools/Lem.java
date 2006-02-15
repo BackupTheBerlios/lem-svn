15094b04645d5a33c275a2cd74fe20dc7c7d4dc7
sjr <sjr@6e2ef900-fc03-0410-88c8-97df3a172d98>
2006-02-15 12:39:09 +0000

    Reformat the entire file to test Eclipse's reformat tool

--- trunk/src/org/jdns/xtuml/tools/Lem.java
+++ trunk/src/org/jdns/xtuml/tools/Lem.java
@@ -36,118 +36,126 @@ import org.jdns.xtuml.parser.Token;
 import org.jdns.xtuml.util.SourceBuffer;
 
 /**
- * A command-line parser for LEM-coded models - LEM: a Language for Executable Models. 
+ * A command-line parser for LEM-coded models - LEM: a Language for Executable
+ * Models.
  * <p>
- * LEM allows xtUML models to be expressed in a text-based language. This LEM parser
- * is the first of a series of tools designed to allow models to be verified and translated into code.
+ * LEM allows xtUML models to be expressed in a text-based language. This LEM
+ * parser is the first of a series of tools designed to allow models to be
+ * verified and translated into code.
  * <p>
- * The LEM parser
- * is a JAVA program which parses the LEM input file (according to the formal grammar) and builds
- * an in-memory graph representing the xtUML model defined by the LEM input file. This software is
- * designed to form the basis of a suite of Open Source tools supporting xtUML.
+ * The LEM parser is a JAVA program which parses the LEM input file (according
+ * to the formal grammar) and builds an in-memory graph representing the xtUML
+ * model defined by the LEM input file. This software is designed to form the
+ * basis of a suite of Open Source tools supporting xtUML.
  * <p>
  * <h2>Acknowledgements</h2>
  * <ol>
- * <li>Emma Margaret Walker, <i>A language for the specification of Shlaer-Mellor object-oriented
- * analysis models</i><br>ANU, BSEng Honours thesis, November 2002 </li>
- * <li>Leon Starr, <i>Metamodel for xtUML, 2002;</i> upon which much of the JAVA object model is based</li>
+ * <li>Emma Margaret Walker, <i>A language for the specification of
+ * Shlaer-Mellor object-oriented analysis models</i><br>
+ * ANU, BSEng Honours thesis, November 2002 </li>
+ * <li>Leon Starr, <i>Metamodel for xtUML, 2002;</i> upon which much of the
+ * JAVA object model is based</li>
  * <li>Shayne Flint, instrumental in providing ongoing support and direction</li>
  * </ol>
- *
- *
+ * 
+ * 
  * <p>
  * Usage:<br>
+ * 
  * <pre>
- * java -jar lem.jar model_File_Path
+ *    java -jar lem.jar model_File_Path
  * </pre>
- *
- * @author  Steven Ring
+ * 
+ * @author Steven Ring
  */
 public class Lem {
-    
-	private static boolean semanticCheck = true;
+
 	private static String filename = null;
+
 	private static SourceBuffer source = null;
 
-	private /*parser.*/Token errorToken = null;
+	private Token errorToken = null;
+
 	public Throwable errorException = null;
+
 	public String errorMessage = "";
-    
+
 	protected LEMModelDeclaration rootNode;
-    
+
 	public static void main(String args[]) {
 
-		Model model = null;
-        
 		// print a copyright notice
 		copyrightNotice();
-		checkOptions( args );
-        
+		checkOptions(args);
+
 		// create a LemParser to parse the input file
-        
+
 		Lem parser = null;
 
 		try {
 			parser = new Lem();
-			model = parser.parse( getInputStream( args ) );
-		    
-			// if no exceptions were thrown, then the input stream is valid LEM
-		    
-			System.err.println("Input file is VALID LEM.");
-		    
-			// all done ... bit of an anti-climax really!
-		} catch ( ParseException e) {
-			System.err.println("Doh! ... got " + e.getClass().getName() );
-		    
-			/*parser.*/Token token = e.currentToken.next;
-			printSourceLine( token );
-			System.err.println(e.getMessage());
-		} catch ( LemException e ) {
+			parser.parse(getInputStream(args));
 
-			System.err.println("Oops! ... got " + e.getClass().getName() );
-		    
-			Token token = e.getToken();
-			printSourceLine( token );
+			// if no exceptions were thrown, then the input stream is valid LEM
+
+			System.err.println("Input file is VALID LEM.");
+
+			// all done ... bit of an anti-climax really!
+		} catch (ParseException e) {
+			System.err.println("Doh! ... got " + e.getClass().getName());
+
+			Token token = e.currentToken.next;
+			printSourceLine(token);
 			System.err.println(e.getMessage());
-			System.err.println( e.getExplanation() );
+		} catch (LemException e) {
+
+			System.err.println("Oops! ... got " + e.getClass().getName());
+
+			Token token = e.getToken();
+			printSourceLine(token);
+			System.err.println(e.getMessage());
+			System.err.println(e.getExplanation());
 
 		} catch (Throwable e) {
-		    
+
 			// report any exception thrown during LEM parsing or model building
-		    
-			System.err.println("Ouch! ... got " + e.getClass().getName() );
+
+			System.err.println("Ouch! ... got " + e.getClass().getName());
 			System.err.println(e.getMessage());
 			e.printStackTrace();
-			usage( null );
+			usage(null);
 		}
-        
-		System.exit( 0 );
+
+		System.exit(0);
 	}
-    
+
 	/**
 	 * Return a parsed model based on the supplied LEM input stream
-	 *
-	 * @param lemStream the model expressed in LEM
+	 * 
+	 * @param lemStream
+	 *            the model expressed in LEM
 	 * @return the model as an instatiated graph of metamodel object instances
-	 * @throws Throwable in the event of an error. A ParseException is thrown
-	 * if parsing fails, a LemException is thrown if the model fails is semantic
-	 * checking fails. Any Throwable may be thrown in the event of a program bug (this
-	 * is a "work in progress" after all!)
+	 * @throws Throwable
+	 *             in the event of an error. A ParseException is thrown if
+	 *             parsing fails, a LemException is thrown if the model fails is
+	 *             semantic checking fails. Any Throwable may be thrown in the
+	 *             event of a program bug (this is a "work in progress" after
+	 *             all!)
 	 */
-	public Model parse( InputStream lemStream ) throws ParseException, LemException, IOException {
+	public Model parse(InputStream lemStream) throws ParseException,
+			LemException, IOException {
 
 		// read the source into a SourceBuffer
 
-		source = new SourceBuffer( lemStream );
+		source = new SourceBuffer(lemStream);
 
 		Model model = null;
-		LemParser parser = new LemParser( source.getInputStream() );
+		LemParser parser = new LemParser(source.getInputStream());
 
 		try {
 
 			model = parser.parse();
 			rootNode = parser.getRootNode();
-
 
 		} catch (ParseException e) {
 
@@ -156,7 +164,7 @@ public class Lem {
 			buildErrorSourceLine();
 			throw e;
 
-		} catch (LemException e ) {
+		} catch (LemException e) {
 
 			errorException = e;
 			errorToken = e.getToken();
@@ -167,41 +175,43 @@ public class Lem {
 
 		return model;
 	}
-    
+
 	/**
 	 * Return a parsed model based on the supplied LEM reader
-	 *
-	 * @param lemReader the model expressed in LEM
+	 * 
+	 * @param lemReader
+	 *            the model expressed in LEM
 	 * @return the model as an instatiated graph of metamodel object instances
-	 * @throws Throwable in the event of an error. A ParseException is thrown
-	 * if parsing fails, a LemException is thrown if the model fails is semantic
-	 * checking fails. Any Throwable may be thrown in the event of a program bug (this
-	 * is a "work in progress" after all!)
+	 * @throws Throwable
+	 *             in the event of an error. A ParseException is thrown if
+	 *             parsing fails, a LemException is thrown if the model fails is
+	 *             semantic checking fails. Any Throwable may be thrown in the
+	 *             event of a program bug (this is a "work in progress" after
+	 *             all!)
 	 */
-	public Model parse( Reader lemReader ) 
-			throws ParseException, LemException, IOException {
+	public Model parse(Reader lemReader) throws ParseException, LemException,
+			IOException {
 		// read the source into a SourceBuffer
 
-		source = new SourceBuffer( lemReader );
+		source = new SourceBuffer(lemReader);
 
 		Model model = null;
-		LemParser parser = new LemParser( source.getInputStream() );
+		LemParser parser = new LemParser(source.getInputStream());
 
 		try {
-		    
+
 			model = parser.parse();
 			rootNode = parser.getRootNode();
 
+		} catch (ParseException e) {
 
-		} catch ( ParseException e) {
-		    
 			errorException = e;
 			errorToken = e.currentToken.next;
 			buildErrorSourceLine();
 			throw e;
 
-		} catch ( LemException e ) {
-		    
+		} catch (LemException e) {
+
 			errorException = e;
 			errorToken = e.getToken();
 			buildErrorSourceLine();
@@ -212,66 +222,65 @@ public class Lem {
 		return model;
 	}
 
-	private static void printSourceLine( Token t ) {
+	private static void printSourceLine(Token t) {
 		int line = 0;
 		int col = 0;
 
-		System.err.print("In file " + filename );
+		System.err.print("In file " + filename);
 
 		if (t == null) {
-			System.err.println( "" );
+			System.err.println("");
 			return;
 		}
 
 		line = t.beginLine;
 		col = t.beginColumn;
-		String sourceLine = source.getLine( line-1 );
-		System.err.println(" at line " + line +", column " + col );
-		System.err.println( sourceLine );
+		String sourceLine = source.getLine(line - 1);
+		System.err.println(" at line " + line + ", column " + col);
+		System.err.println(sourceLine);
 
 		// now print a pointer to the position
 
 		StringBuffer sb = new StringBuffer();
-		for ( int i = 0; i < col-1; i++ )
-			sb.append( " " );
-		sb.append( "|" );
-		System.err.println( sb.toString() );
+		for (int i = 0; i < col - 1; i++)
+			sb.append(" ");
+		sb.append("|");
+		System.err.println(sb.toString());
 
 	}
 
 	/**
-	 * Build the error message using the errorException and
-	 * errorToken
+	 * Build the error message using the errorException and errorToken
 	 */
 	private void buildErrorSourceLine() {
 		int line = 0;
 		int col = 0;
 		StringBuffer s = new StringBuffer();
 
-		s.append( "In file " + filename + "\n" );
-		if ( errorToken != null ) {
+		s.append("In file " + filename + "\n");
+		if (errorToken != null) {
 			line = errorToken.beginLine;
 			col = errorToken.beginColumn;
-			String sourceLine = source.getLine( line-1 );
-			s.append(" at line " + line +", column " + col + "\n" );
-			s.append( sourceLine + "\n" );
+			String sourceLine = source.getLine(line - 1);
+			s.append(" at line " + line + ", column " + col + "\n");
+			s.append(sourceLine + "\n");
 
 			// now print a pointer to the position
 
 			StringBuffer sb = new StringBuffer();
-			for ( int i = 0; i < col-1; i++ )
-				sb.append( " " );
-			sb.append( "|" );
-			s.append( sb.toString() + "\n" );
+			for (int i = 0; i < col - 1; i++)
+				sb.append(" ");
+			sb.append("|");
+			s.append(sb.toString() + "\n");
 
 		} else {
-			s.append( "\n" );
+			s.append("\n");
 		}
 
-		s.append(errorException.getMessage() + "\n" );
-		if ( errorException instanceof LemException ) {
+		s.append(errorException.getMessage() + "\n");
+		if (errorException instanceof LemException) {
 			LemException le = (LemException) errorException;
-			s.append( le.getExplanation() + "\n" );
+			s.append(le.getExplanation() + "\n");
 		}
 
 		// store the resulting error message
@@ -279,46 +288,42 @@ public class Lem {
 		errorMessage = s.toString();
 	}
 
-
-
 	/**
 	 * Return the input stream determined from supplied parameter line arguments
-	 *
-	 * @param args the command line arguments
+	 * 
+	 * @param args
+	 *            the command line arguments
 	 * @return an InputStream containing the model to be parsed
 	 */
-	private static InputStream getInputStream( String [] args ) throws Exception {
+	private static InputStream getInputStream(String[] args) throws Exception {
 		// select the input stream to read from
 
 		InputStream in = null;
 
 		// read from file if filename is provided
 
-		if ( filename != null ) {
+		if (filename != null) {
 			URL u = null;
 			try {
-				u = new URL( filename );
-			} catch( java.net.MalformedURLException m ) {
+				u = new URL(filename);
+			} catch (java.net.MalformedURLException m) {
 				u = null;
 			}
 
 			try {
-				if( u == null ) {
-					// Try opening the file with the given 
+				if (u == null) {
+					// Try opening the file with the given
 					// filename
-					in = new FileInputStream( filename );
+					in = new FileInputStream(filename);
 				} else {
 					in = u.openStream();
 				}
-			} catch( IOException e ) {
-				String err = (u == null) 
-						? "reading from"
-						: "opening";
-				System.err.println( "There was an error " 
-					+ err + " the given URL"
-					+ ": " + e.getMessage());
-				usage( null );
-				System.exit( 1 );
+			} catch (IOException e) {
+				String err = (u == null) ? "reading from" : "opening";
+				System.err.println("There was an error " + err
+						+ " the given URL" + ": " + e.getMessage());
+				usage(null);
+				System.exit(1);
 			}
 		} else {
 
@@ -337,62 +342,60 @@ public class Lem {
 	private static void copyrightNotice() {
 
 		String msg = "LEM - a language for executable models\n"
-			+ LemParser.getRelease() 
-			+ " (rev: " + getRevision() + ")\n\n"
-			+ "Copyright (c) 2006 Steven Ring "
-			+ "(steven@southsky.com.au)\n"
-			+ "LEM comes with ABSOLUTELY NO WARRANTY. " 
-			+ "This is free software, and you are welcome "
-			+ "to redistribute it under the conditions "
-			+ "of the GNU General Public Licence "
-			+ "version 2.\n";
+				+ LemParser.getRelease() + " (rev: " + getRevision() + ")\n\n"
+				+ "Copyright (c) 2006 Steven Ring "
+				+ "(steven@southsky.com.au)\n"
+				+ "LEM comes with ABSOLUTELY NO WARRANTY. "
+				+ "This is free software, and you are welcome "
+				+ "to redistribute it under the conditions "
+				+ "of the GNU General Public Licence " + "version 2.\n";
 
-		System.out.println( msg );
+		System.out.println(msg);
 	}
 
 	/**
 	 * Return the revision string
-	 *
+	 * 
 	 * @return the revision string
 	 */
 	public static String getRevision() {
 
 		String revision = LemParser.getRevision();
-		String [] substrings = revision.split( " " );
-		return( substrings[1] );
+		String[] substrings = revision.split(" ");
+		return (substrings[1]);
 	}
 
 	/**
 	 * Check parameters
 	 */
-	private static void checkOptions( String [] args ) {
-		for( int i = 0; i < args.length ; i++ ) {
-			if (args[i].equals("-s") )
-				semanticCheck = false;
-			else
+	private static void checkOptions(String[] args) {
+		for (int i = 0; i < args.length; i++) {
+			if (args[i].equals("-s")) {
+			} else
 				filename = args[i];
 		}
 	}
 
-
 	/**
 	 * Print a usage message
-	 *
-	 * @param aMessage to be displayed
+	 * 
+	 * @param aMessage
+	 *            to be displayed
 	 */
-	private static void usage( String aMessage ) {
+	private static void usage(String aMessage) {
 
-		if ( null != aMessage ) {
-			System.err.println( "\n" + aMessage +"\n" );
+		if (null != aMessage) {
+			System.err.println("\n" + aMessage + "\n");
 		}
 
 		// now the usage text
 
-		System.err.println( "Usage:\n" );
-		System.err.println( "java Lem <options> [model_filename]\n" );
-		System.err.println( "options:" );
-		System.err.println( "   -s for syntax only check" );
-		System.err.println( "If a model filename is omitted, Lem will read from standard input" );
+		System.err.println("Usage:\n");
+		System.err.println("java Lem <options> [model_filename]\n");
+		System.err.println("options:");
+		System.err.println("   -s for syntax only check");
+		System.err
+				.println("If a model filename is omitted, Lem will read from standard input");
 	}
 
 	public LEMModelDeclaration getRootNode() {
