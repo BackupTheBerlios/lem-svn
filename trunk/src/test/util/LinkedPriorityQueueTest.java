4702a720230c6171733beaf8b58cb67e99fc9f73
sjr <sjr@6e2ef900-fc03-0410-88c8-97df3a172d98>
2006-02-15 12:47:34 +0000

    Fix compilation and reformat

--- trunk/src/test/util/LinkedPriorityQueueTest.java
+++ trunk/src/test/util/LinkedPriorityQueueTest.java
@@ -9,56 +9,61 @@ package util;
 
 import org.jdns.xtuml.util.LinkedPriorityQueue;
 
-import org.jdns.xtuml.util.LinkedPriorityQueue;
-
 public class LinkedPriorityQueueTest extends junit.framework.TestCase {
 
-	Integer one = new Integer( 1 );
-	Integer two = new Integer( 2 );
-	Integer three = new Integer( 3 );
-	Integer four = new Integer( 4 );
-	Integer five = new Integer( 5 );
-	Integer six = new Integer( 6 );
+	Integer one = new Integer(1);
+
+	Integer two = new Integer(2);
+
+	Integer three = new Integer(3);
+
+	Integer four = new Integer(4);
+
+	Integer five = new Integer(5);
+
+	Integer six = new Integer(6);
 
 	public LinkedPriorityQueueTest(String testName) {
 		super(testName);
 	}
 
 	public void testMinQueue() {
-		LinkedPriorityQueue q = new LinkedPriorityQueue( LinkedPriorityQueue.MIN_QUEUE );
+		LinkedPriorityQueue q = new LinkedPriorityQueue(
+				LinkedPriorityQueue.MIN_QUEUE);
 
-		q.push( four );
-		q.push( three );
-		q.push( five );
-		assertEquals( "3 should come off first", three, q.pop() );
-		assertEquals( "4 next", four, q.pop() );
-		assertEquals( "5 next", five, q.pop() );
-
-		q.clear();
-
-		q.push( five );
-		assertEquals( "5 should be the only element of the list", five, q.pop() );
+		q.add(four);
+		q.add(three);
+		q.add(five);
+		assertEquals("3 should come off first", three, q.pop());
+		assertEquals("4 next", four, q.pop());
+		assertEquals("5 next", five, q.pop());
 
 		q.clear();
-		assertEquals( "Popping an empty queue gives null", null, q.pop() );
+
+		q.add(five);
+		assertEquals("5 should be the only element of the list", five, q.pop());
+
+		q.clear();
+		assertEquals("Popping an empty queue gives null", null, q.pop());
 	}
 
 	public void testMaxQueue() {
-		LinkedPriorityQueue q = new LinkedPriorityQueue( LinkedPriorityQueue.MAX_QUEUE );
+		LinkedPriorityQueue q = new LinkedPriorityQueue(
+				LinkedPriorityQueue.MAX_QUEUE);
 
-		q.push( four );
-		q.push( three );
-		q.push( five );
-		assertEquals( "5 should come off first", five, q.pop() );
-		assertEquals( "4 next", four, q.pop() );
-		assertEquals( "3 next", three, q.pop() );
-
-		q.clear();
-
-		q.push( five );
-		assertEquals( "5 should be the only element of the list", five, q.pop() );
+		q.add(four);
+		q.add(three);
+		q.add(five);
+		assertEquals("5 should come off first", five, q.pop());
+		assertEquals("4 next", four, q.pop());
+		assertEquals("3 next", three, q.pop());
 
 		q.clear();
-		assertEquals( "Popping an empty queue gives null", null, q.pop() );
+
+		q.add(five);
+		assertEquals("5 should be the only element of the list", five, q.pop());
+
+		q.clear();
+		assertEquals("Popping an empty queue gives null", null, q.pop());
 	}
 }
