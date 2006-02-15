961cd45094a190f0b06b139a76d528a4511c71d2
sjr <sjr@6e2ef900-fc03-0410-88c8-97df3a172d98>
2006-02-15 12:48:48 +0000

    Fix compilation and reformat

--- trunk/src/test/perlgen/ValueObjectGenerator.java
+++ trunk/src/test/perlgen/ValueObjectGenerator.java
@@ -16,143 +16,125 @@ import java.io.*;
 
 /**
  * Create PERL value objects from a LEM model supplied on standard in
- *
- * @author  smr
+ * 
+ * @author smr
  */
 public class ValueObjectGenerator {
-    
-    /** Creates a new instance of ValueObjectGenerator */
-    public ValueObjectGenerator() {
-    }
-    
-    public static void main( String [] args ) {
-        
-        // parse the supplied model
-        
-        Lem parser = null;
-        try {
-            parser = new Lem();
-            Model model = parser.parse( System.in );
 
-			listDomains( model );
-            
-        } catch ( Exception e) {
+	/** Creates a new instance of ValueObjectGenerator */
+	public ValueObjectGenerator() {
+	}
 
-            System.err.println( "Doh! ... got " + e.getClass().getName() );
-            System.err.println( parser.errorMessage );
-			System.err.println( parser.errorMessage );
-            e.printStackTrace();
+	public static void main(String[] args) {
 
+		// parse the supplied model
 
-        } catch (Throwable e) {
+		Lem parser = null;
+		try {
+			parser = new Lem();
+			Model model = parser.parse(System.in);
 
-            // report any exception thrown during LEM parsing or model building
-            
-            System.err.println("Ouch! ... got " + e.getClass().getName() );
-            System.err.println(e.getMessage());
-            e.printStackTrace();
-        }
-    }
+			listDomains(model);
+
+		} catch (Exception e) {
+
+			System.err.println("Doh! ... got " + e.getClass().getName());
+			System.err.println(parser.errorMessage);
+			System.err.println(parser.errorMessage);
+			e.printStackTrace();
+
+		} catch (Throwable e) {
+
+			// report any exception thrown during LEM parsing or model building
+
+			System.err.println("Ouch! ... got " + e.getClass().getName());
+			System.err.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
 
 	/**
 	 * List some elements
 	 */
-	private static void listDomains( Model model ) throws IOException {
+	private static void listDomains(Model model) throws IOException {
 
 		HashMap domains = model.getDomains();
 
-		System.out.println( "Model is called: " + model.getName());
-		for ( Iterator it = domains.values().iterator(); it.hasNext(); ) {
+		System.out.println("Model is called: " + model.getName());
+		for (Iterator it = domains.values().iterator(); it.hasNext();) {
 			Domain domain = (Domain) it.next();
-			listClasses( domain );
+			listClasses(domain);
 		}
 	}
-			
-    
+
 	/**
 	 * List some classes
 	 */
-	private static void listClasses( Domain domain ) throws IOException {
+	private static void listClasses(Domain domain) throws IOException {
 
 		HashMap classes = domain.getClasses();
-              
-                
-                File outDir = new File( "/tmp/");
-		System.out.println( "Domain is called: " + domain.getName());
-		for ( Iterator it = classes.values().iterator(); it.hasNext(); ) {
-			org.jdns.xtuml.metamodel.Class theClass = (org.jdns.xtuml.metamodel.Class) it.next();
-			listAttributes( theClass );
-                        buildPackage( theClass, outDir );
+
+		File outDir = new File("/tmp/");
+		System.out.println("Domain is called: " + domain.getName());
+		for (Iterator it = classes.values().iterator(); it.hasNext();) {
+			org.jdns.xtuml.metamodel.Class theClass = (org.jdns.xtuml.metamodel.Class) it
+					.next();
+			listAttributes(theClass);
+			buildPackage(theClass, outDir);
 		}
 	}
-        
-        
-        /**
+
+	/**
 	 * List the attributes
 	 */
-	private static void listAttributes( org.jdns.xtuml.metamodel.Class theClass ) {
+	private static void listAttributes(org.jdns.xtuml.metamodel.Class theClass) {
 
 		Collection attributes = theClass.getAllAttributes();
 
-		System.out.println( "Class is " + theClass.getName() );
-               
-                
-		for ( Iterator it = attributes.iterator(); it.hasNext(); ) {
+		System.out.println("Class is " + theClass.getName());
+
+		for (Iterator it = attributes.iterator(); it.hasNext();) {
 			Attribute attribute = (Attribute) it.next();
-			System.out.println( "  attribute: " + attribute.getName() );
+			System.out.println("  attribute: " + attribute.getName());
 		}
 	}
-        
-        private static void buildPackage( org.jdns.xtuml.metamodel.Class theClass, File outDir )
-            throws IOException {
-            
-            
-            // create the file for the output source
-            
-            String className = theClass.getName();
-           /* 
-            
-            PrintWriter out = new PrintWriter( outDir.getAbsolutePath() + "/" + className + ".pm" );
-            
-            out.println( "use Object;");
-            out.println( "use XMLserV2;");
-            out.println( "use Carp;");
-            
-            
-            
-            out.println("package " + className +";");
-            
-            out.println( "@ISA = qw( Object XMLserV2 );");
-            out.println( "Ser::registerClass( \"" + className +"\" );" );
 
-            out.println( "\n# Attribute definitions\n");
-            
-            HashMap attributes = theClass.getAttributes();
-            out.println( "my %fields = (" );
-            for ( Iterator it = attributes.values().iterator(); it.hasNext(); ) {
-		Attribute attribute = (Attribute) it.next();
-		out.print( "    " + attribute.getName() + " => undef" );
-                if ( it.hasNext() )
-                    out.println( "," );
-            }
-            out.println( "\n);");
-            
-            // standard PERL constructor
-            
-            out.println( "\n# Default constructor\n");
-            out.println( 
-                "sub new {\n" +
-                "    my $class = shift;\n" +
-                "    my $self = { %fields };\n" +
-                "    bless $self, $class;\n" +
-                "    return $self;\n" +
-                "}");
-            
-            // close the source file
-            
-            out.flush();
-            out.close();
-	    */
-        }
-	
+	private static void buildPackage(org.jdns.xtuml.metamodel.Class theClass,
+			File outDir) throws IOException {
+
+		// create the file for the output source
+
+		String className = theClass.getName();
+		/*
+		 * 
+		 * PrintWriter out = new PrintWriter( outDir.getAbsolutePath() + "/" +
+		 * className + ".pm" );
+		 * 
+		 * out.println( "use Object;"); out.println( "use XMLserV2;");
+		 * out.println( "use Carp;");
+		 * 
+		 * 
+		 * 
+		 * out.println("package " + className +";");
+		 * 
+		 * out.println( "@ISA = qw( Object XMLserV2 );"); out.println(
+		 * "Ser::registerClass( \"" + className +"\" );" );
+		 * 
+		 * out.println( "\n# Attribute definitions\n");
+		 * 
+		 * HashMap attributes = theClass.getAttributes(); out.println( "my
+		 * %fields = (" ); for ( Iterator it = attributes.values().iterator();
+		 * it.hasNext(); ) { Attribute attribute = (Attribute) it.next();
+		 * out.print( " " + attribute.getName() + " => undef" ); if (
+		 * it.hasNext() ) out.println( "," ); } out.println( "\n);"); //
+		 * standard PERL constructor
+		 * 
+		 * out.println( "\n# Default constructor\n"); out.println( "sub new {\n" + "
+		 * my $class = shift;\n" + " my $self = { %fields };\n" + " bless $self,
+		 * $class;\n" + " return $self;\n" + "}"); // close the source file
+		 * 
+		 * out.flush(); out.close();
+		 */
+	}
+
 }
